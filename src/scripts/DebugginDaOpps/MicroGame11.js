d391839f3a43569c567941e828b327cb1550cbd6
kyrie <echolocks@gmail.com>
2022-07-12 17:54:04 -0400

    added assets

--- src/scripts/DebugginDaOpps/MicroGame11.js
+++ src/scripts/DebugginDaOpps/MicroGame11.js
@@ -1,42 +1,57 @@
 export default class MicroGame11 extends Phaser.Scene {
-    // Game Class Constructor
-    constructor() {
-        super({
-            active: false,
-            visible: false,
-            key: 'MicroGame11',
-        });
+  // Game Class Constructor
+  constructor() {
+    super({
+      active: false,
+      visible: false,
+      key: "MicroGame11",
+    });
 
-        // Game Object Declarations
-        this.myText;
-        this.eventScreen;
-        this.startText;
+    // Game Object Declarations
+    this.myText;
+    this.eventScreen;
+    this.startText;
+  }
 
+  preload() {
+    console.log("preload start");
+    this.load.image(
+      "background",
+      new URL("./assets/background.png", import.meta.url).href
+    );
+    this.load.image(
+      "full-tire",
+      newURL("./assets/car.png", import.meta.url).href
+    );
+    this.load.image("car", new URL("./assets/car.png", import.meta.url).href);
+  }
 
-    }
+  create() {
+    console.log("create");
+    this.add.image(540, 360, "background");
+    this.setText();
+    // timer = game.time.create(false);
+    timedEvent = this.time.addEvent({
+      delay: 500,
+      callback: onEvent,
+      callbackScope: this,
+      loop: false,
+    });
+  }
 
-    preload() {
-        console.log("preload start");
-        this.load.image('background', new URL('./assets/background.png', import.meta.url).href);
-    }
+  update() {}
 
-    create() {
-        console.log("create")
-        this.add.image(540, 360, 'background');
-        this.setText();
-        // timer = game.time.create(false);
-    }
+  setText() {
+    this.startText = this.add.text(360, 300, "");
+    this.startText.setStyle({
+      fontSize: "100px",
+      fill: "#000000",
+      align: "center",
+    });
+    this.startText.setText("PUMP!");
+  }
 
-    update() {
-    }
-
-    setText() {
-        this.startText = this.add.text(360, 300, '')
-        this.startText.setStyle({
-            fontSize: '100px',
-            fill: '#000000',
-            align: 'center',
-        });
-        this.startText.setText('PUMP!');
-    }
+  // onEvent({
+  //     this.startText.visible = false;
+  // });
 }
