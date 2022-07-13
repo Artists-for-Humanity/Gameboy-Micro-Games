f1094a29cccae4e9887f7d71512db5a801cfd80d
jolia <99293077+joliats@users.noreply.github.com>
2022-07-13 13:25:54 -0400

    Debuggin da opps (#2)
    * Update main.js
    
    * added start prompt
    
    * added assets
    
    * added timer event
    
    * pump event added
    
    * Update main.js
    
    * Update main.js
    
    * Delete main.js
    
    * added lever img
    
    * added main.js back
    
    * updated main.js
    
    Co-authored-by: kairo8735 <83092308+kairo8735@users.noreply.github.com>
    Co-authored-by: kyrie <echolocks@gmail.com>

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,23 +1,19 @@
 // Add imports for phaser library, scenes, and plugins
-import Phaser from 'phaser';
-import GlobalState from './GlobalState';
-import MicroGame01 from './8Bitties/MicroGame01';
-import MicroGame11 from './DebugginDaOpps/MicroGame11';
-import MicroGame21 from './MicroGamers/MicroGame21';
-import MicroGame31 from './Team4/MicroGame31';
-
-
-
+import Phaser from "phaser";
+import GlobalState from "./GlobalState";
+import MicroGame01 from "./8Bitties/MicroGame01";
+import MicroGame11 from "./DebugginDaOpps/MicroGame11";
+import MicroGame21 from "./MicroGamers/MicroGame21";
+import MicroGame31 from "./Team4/MicroGame31";
 // Set configuration for phaser game instance
 const config = {
   type: Phaser.AUTO,
   width: 1080,
   height: 720,
-  backgroundColor: '#808080',
-
+  backgroundColor: "#808080",
   // Add physics, arcade, scene, and audio
   physics: {
-    default: 'arcade',
+    default: "arcade",
     arcade: {
       gravity: {
         y: 0,
@@ -32,14 +28,13 @@ const config = {
   plugins: {
     global: [
       {
-        key: 'GlobalState',
+        key: "GlobalState",
         plugin: GlobalState,
         start: false,
-        mapping: 'globalState'
-      }
+        mapping: "globalState",
+      },
     ],
   },
 };
-
 // Initialize game instance
 new Phaser.Game(config);
