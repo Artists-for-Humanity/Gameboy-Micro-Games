4d10bbfc405048e67b50937ff55057cc17e7dab5
gordanliu <gordonliu2004@gmail.com>
2022-07-13 17:34:05 -0400

    adjustments + loss condition

--- src/scripts/MicroGamers/MicroGame21.js
+++ src/scripts/MicroGamers/MicroGame21.js
@@ -8,13 +8,16 @@ export default class MicroGame21 extends Phaser.Scene {
         });
 
         // Game Object Declarations
+        this.background;
+        this.box;
+
         this.myText;
         this.gameState = false;
         this.started = false;
         this.startTimer = 0;
         this.red, this.yellow, this.purple, this.blue;
         this.colorblocks = [];
-        this.originalColors = ["0xFF8B8E", "0xFFE058", "0xB25AFD", "0x4D7DFF"];
+        this.originalColors = ["0xFF8B8E", "0xF3E260", "0xB25AFD", "0x7CAFFE"];
         this.brighterColors = ["0xFF3D41", "0xFFD000", "0x961BFF", "0x001EFF"];
         this.pattern = [];
   
@@ -38,30 +41,40 @@ export default class MicroGame21 extends Phaser.Scene {
     }
 
     preload() {
+        this.load.image('brickWall', new URL('brickWall.png', import.meta.url).href);
+        this.load.image('box', new URL('box.png', import.meta.url).href);
+        this.load.image('redButton', new URL('redButton.png', import.meta.url).href);
+        this.load.image('yellowButton', new URL('yellowButton.png', import.meta.url).href);
+        this.load.image('purpleButton', new URL('purpleButton.png', import.meta.url).href);
+        this.load.image('blueButton', new URL('blueButton.png', import.meta.url).href);
     }
 
     create() {
+        this.background = this.add.image(540, 360, 'brickWall');
+        this.box = this.add.image(540, 360, 'box');
+        this.box.visible = false;
+
         this.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
         this.keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
         this.keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
 
         this.setText(); //start screen text
-        
-        this.red = this.add.rectangle(405, 270, 270, 180, "0xFF8B8E");
-        this.yellow = this.add.rectangle(675, 270, 270, 180, "0xFFE058");
-        this.purple = this.add.rectangle(405, 450, 270, 180, "0xB25AFD");
-        this.blue = this.add.rectangle(675, 450, 270, 180, "0x4D7DFF");
-        this.colorblocks = [this.red, this.yellow, this.purple, this.blue]; //add colorblocks to array
 
-        this.g1 = this.add.rectangle(337.5, 130, 135, 90, "", 0);
-        this.g2 = this.add.rectangle(472.5, 130, 135, 90, "", 0);
-        this.g3 = this.add.rectangle(607.5, 130, 135, 90, "", 0);
-        this.g4 = this.add.rectangle(742.5, 130, 135, 90, "", 0);
-        this.guessBlocks = [this.g1, this.g2, this.g3, this.g4]; //add guess blocks to array, transparent at start
+        this.red = this.add.rectangle(408, 334, 255, 170, "0xFF8B8E");
+        this.yellow = this.add.rectangle(678, 334, 255, 170, "0xF3E260");
+        this.purple = this.add.rectangle(408, 522, 255, 170, "0xB25AFD");
+        this.blue = this.add.rectangle(678, 522, 255, 170, "0x7CAFFE");
+        this.colorblocks = [this.red, this.yellow, this.purple, this.blue]; //add colorblocks to array
 
         for (var i = 0; i < this.colorblocks.length; i++) { //set board invisible when game starts
             this.colorblocks[i].visible = false;
         }
+
+        this.g1 = this.add.rectangle(340.5, 160, 130, 90, "", 0);
+        this.g2 = this.add.rectangle(475.5, 160, 130, 90, "", 0);
+        this.g3 = this.add.rectangle(610.5, 160, 130, 90, "", 0);
+        this.g4 = this.add.rectangle(745.5, 160, 130, 90, "", 0);
+        this.guessBlocks = [this.g1, this.g2, this.g3, this.g4]; //add guess blocks to array, transparent at start
 
         for (var i = 0; i < 4; i++) { //create pattern
             this.pattern.push(this.getRandomInt(4));
@@ -70,28 +83,19 @@ export default class MicroGame21 extends Phaser.Scene {
     }
 
     update(time, delta) {
-        if (!(this.started)) this.startTimer += delta; //only use startTimer if game not started
+        if (!this.started) this.startTimer += delta; //only use startTimer if game not started
         if (this.startTimer >= 2000 && !this.started) { //start game after 2 seconds
-            this.gameState = true;
-            this.started = true;
-        }
-        if (this.gameState) {
             this.startGame();
-            this.gameState = false; //prevents startGame from continuously running
+            this.gameState = true;
+            this.started = true; //prevents startGame from continuously running
         }
-        if (this.interactive) { //interactive modde on
+        if (this.interactive && this.gameState === true) { //only runs when interactive mode is turned on AND gameState is TRUE
             this.clickTimer += delta;
             this.selectTimer += delta;
             if (this.clickTimer >= 200) this.clickAvailable = true; //only allows for click every 200ms
             if (this.selectTimer >= 200) this.selectAvailable = true; //only allows for selection every 200ms
             this.userInput();
-
-            if (JSON.stringify(this.guesses) == JSON.stringify(this.pattern) && !this.win) {
-                console.log("YOU WIN!");
-                this.win = true;
-            }
         }
-
     }
 
     setText() {
@@ -107,11 +111,12 @@ export default class MicroGame21 extends Phaser.Scene {
     startGame() {
         this.myText.visible = false;
         this.showBoard();
-        this.time.delayedCall(500, this.flashPattern, [], this);
+        this.time.delayedCall(1000, this.flashPattern, [], this);
         this.time.delayedCall(this.pattern.length * 500 + 1000, this.startInteractive, [], this);
     }
 
     showBoard() {
+        this.box.visible = true;
         for (var i = 0; i < this.colorblocks.length; i++) {
             this.colorblocks[i].visible = true;
         }
@@ -172,13 +177,21 @@ export default class MicroGame21 extends Phaser.Scene {
             this.clickAvailable = false;
         } else if (this.selectAvailable && this.keySPACE.isDown && this.guesses.length <= 3) {
             this.guesses.push(this.selected);
-            this.guessBlocks[this.guess].setFillStyle(this.originalColors[this.guesses[this.guess]], 1);
-            this.guessBlocks[this.guess].setStrokeStyle(2, "0x000000", 1);
+            this.guessBlocks[this.guess].setFillStyle(this.originalColors[this.guesses[this.guess]]);
+            this.guessBlocks[this.guess].setStrokeStyle(2, "0x000000");
+            console.log(this.guesses);
+            if (JSON.stringify(this.guesses[this.guess]) != JSON.stringify(this.pattern[this.guess])) { //check loss condition
+                console.log("YOU LOSE");
+                this.gameState = false;
+            }
+            if (JSON.stringify(this.guesses) == JSON.stringify(this.pattern) && !this.win) { //check win condition
+                console.log("YOU WIN!");
+                this.gameState = false;
+                this.win = true;
+            }
             this.guess++;
             this.selectTimer = 0;
-            this.selectAvailable = false;
-            console.log(this.guesses);
-            
+            this.selectAvailable = false;            
         }
     }
 }
