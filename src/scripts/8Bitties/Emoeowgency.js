f32e3097bbc0de74900bd8a7c45744d251fbb686
VillainMythic <87036074+VillainMythic@users.noreply.github.com>
2022-07-13 11:46:19 -0400

    renaming and minor cleanup
    removing console logs etc.

--- src/scripts/8Bitties/Emoeowgency.js
+++ src/scripts/8Bitties/Emoeowgency.js
@@ -1,10 +1,10 @@
-export default class MicroGame01 extends Phaser.Scene {
+export default class Emoeowgency extends Phaser.Scene {
   // Game Class Constructor
   constructor() {
     super({
       active: false,
       visible: false,
-      key: "MicroGame01",
+      key: "Emeowgency",
     });
     this.gamestart = false;
     // Game Object Decla
@@ -16,51 +16,42 @@ export default class MicroGame01 extends Phaser.Scene {
   preload() {
     this.load.image(
       "catch",
-      new URL("../8Bitties/assets/Catch_Text.png",
-        import.meta.url).href
+      new URL("../8Bitties/assets/Catch_Text.png", import.meta.url).href
     );
     this.load.image(
       "fail",
-      new URL("../8Bitties/assets/fail_text.png",
-        import.meta.url).href
+      new URL("../8Bitties/assets/fail_text.png", import.meta.url).href
     );
     this.load.image(
       "safe",
-      new URL("../8Bitties/assets/safe_text.png",
-        import.meta.url).href
+      new URL("../8Bitties/assets/safe_text.png", import.meta.url).href
     );
     this.load.image(
       "shadow",
-      new URL("../8Bitties/assets/animations/shadow.png",
-        import.meta.url).href
+      new URL("../8Bitties/assets/animations/shadow.png", import.meta.url).href
     );
     this.load.image(
       "yang_fall",
-      new URL("../8Bitties/assets/animations/yang_fall.png",
-        import.meta.url)
+      new URL("../8Bitties/assets/animations/yang_fall.png", import.meta.url)
         .href
     );
     this.load.image(
       "yang_safe",
-      new URL("../8Bitties/assets/animations/yang_safe.png",
-        import.meta.url)
+      new URL("../8Bitties/assets/animations/yang_safe.png", import.meta.url)
         .href
     );
     this.load.image(
       "yang_falling",
-      new URL("../8Bitties/assets/animations/Yang_Falling.png",
-        import.meta.url)
+      new URL("../8Bitties/assets/animations/Yang_Falling.png", import.meta.url)
         .href
     );
     this.load.image(
       "Blanket",
-      new URL("../8Bitties/assets/blanket.png",
-        import.meta.url).href
+      new URL("../8Bitties/assets/blanket.png", import.meta.url).href
     );
     this.load.image(
       "grass",
-      new URL("../8Bitties/assets/grass_bg.png",
-        import.meta.url).href
+      new URL("../8Bitties/assets/grass_bg.png", import.meta.url).href
     );
   }
 
@@ -76,6 +67,7 @@ export default class MicroGame01 extends Phaser.Scene {
   }
   gameStart() {
     this.grass = this.add.image(490, 360, "grass");
+    this.spawnCat();
   }
   getRandomInt(max) {
     return Math.floor(Math.random() * max);
@@ -94,14 +86,12 @@ export default class MicroGame01 extends Phaser.Scene {
     console.log(this.catchScale);
     if (this.catchScale <= 0.9) {
       this.timer++;
-      console.log(this.timer)
-      this.catch.setScale(this.catchScale);
       this.catchScale += 0.2 / this.timer;
+      this.catch.setScale(this.catchScale);
     } else if (this.timer === 137) {
       this.catch.destroy();
       this.gameStart();
       this.timer = 0;
-      console.log('hi');
     }
   }
 }
---
+++ src/scripts/8Bitties/sprites/cat.js
@@ -0,0 +1,3 @@
+export default class Cat extends Phaser.Physics.Arcade.Sprite {
+  constructor(scene, x, y) {}
+}
