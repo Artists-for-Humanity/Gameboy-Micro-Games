fd3936470317beb385731b90e0979982b6a57838
VillainMythic <87036074+VillainMythic@users.noreply.github.com>
2022-07-19 12:13:46 -0400

    empty commit

--- src/scripts/8Bitties/TugOWar.js
+++ src/scripts/8Bitties/TugOWar.js
@@ -16,51 +16,42 @@ export default class TugOWar extends Phaser.Scene {
   preload() {
     this.load.image(
       "catch",
-      new URL("../8Bitties/assets/Catch_Text.png",
-        import.meta.url).href
+      new URL("../8Bitties/assets/Catch_Text.png", import.meta.url).href
     );
     this.load.image(
       "fail",
-      new URL("../8Bitties/assets/fail_text.png",
-        import.meta.url).href
+      new URL("../8Bitties/assets/fail_text.png", import.meta.url).href
     );
     this.load.image(
       "safe",
-      new URL("../8Bitties/assets/safe_text.png",
-        import.meta.url).href
+      new URL("../8Bitties/assets/safe_text.png", import.meta.url).href
     );
     this.load.image(
       "shadow",
-      new URL("../8Bitties/assets/animations/shadow.png",
-        import.meta.url).href
+      new URL("../8Bitties/assets/animations/shadow.png", import.meta.url).href
     );
     this.load.image(
       "yang_fall",
-      new URL("../8Bitties/assets/animations/yang_fall.png",
-        import.meta.url)
+      new URL("../8Bitties/assets/animations/yang_fall.png", import.meta.url)
         .href
     );
     this.load.image(
       "yang_safe",
-      new URL("../8Bitties/assets/animations/yang_safe.png",
-        import.meta.url)
+      new URL("../8Bitties/assets/animations/yang_safe.png", import.meta.url)
         .href
     );
     this.load.image(
       "yang_falling",
-      new URL("../8Bitties/assets/animations/Yang_Falling.png",
-        import.meta.url)
+      new URL("../8Bitties/assets/animations/Yang_Falling.png", import.meta.url)
         .href
     );
     this.load.image(
       "Blanket",
-      new URL("../8Bitties/assets/blanket.png",
-        import.meta.url).href
+      new URL("../8Bitties/assets/blanket.png", import.meta.url).href
     );
     this.load.image(
       "grass",
-      new URL("../8Bitties/assets/grass_bg.png",
-        import.meta.url).href
+      new URL("../8Bitties/assets/grass_bg.png", import.meta.url).href
     );
   }
 
@@ -94,14 +85,14 @@ export default class TugOWar extends Phaser.Scene {
     console.log(this.catchScale);
     if (this.catchScale <= 0.9) {
       this.timer++;
-      console.log(this.timer)
+      console.log(this.timer);
       this.catch.setScale(this.catchScale);
       this.catchScale += 0.2 / this.timer;
     } else if (this.timer === 137) {
       this.catch.destroy();
       this.gameStart();
       this.timer = 0;
-      console.log('hi');
+      console.log("hi");
     }
-  }
+  } //empty commit//
 }
