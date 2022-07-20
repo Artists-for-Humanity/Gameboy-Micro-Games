97722f00480596fae34119f869b46f200198a911
VillainMythic <87036074+VillainMythic@users.noreply.github.com>
2022-07-20 17:55:39 -0400

    finnished working on majority of tug-o-war
    (need animation for certain things, still missing assets)

--- src/scripts/8Bitties/TugOWar.js
+++ src/scripts/8Bitties/TugOWar.js
@@ -22,6 +22,15 @@ export default class TugOWar extends Phaser.Scene {
     this.greenzone;
     this.gamestarted = false;
     this.progNum = 0;
+    this.youlose;
+    this.loseScale = 0;
+    this.LoseTimer = 0;
+    this.youwin;
+    this.winTimer = 0;
+    this.winScale = 0;
+    this.imageCreated = false;
+
+    this.loseDecrease = false;
   }
   preload() {
     this.load.image(
@@ -40,17 +49,32 @@ export default class TugOWar extends Phaser.Scene {
     );
     this.load.image(
       "player",
-      new URL("../8Bitties/assets/TugOWar/imageholder.png", import.meta.url)
-        .href
+      new URL("../8Bitties/assets/TugOWar/person.png", import.meta.url).href
     );
     this.load.image(
       "npc",
-      new URL("../8Bitties/assets/TugOWar/imageholder.png", import.meta.url)
-        .href
+      new URL("../8Bitties/assets/TugOWar/person.png", import.meta.url).href
     );
     this.load.image(
       "rope",
+      new URL("../8Bitties/assets/TugOWar/rope.png", import.meta.url).href
+    );
+    this.load.image(
+      "win",
       new URL("../8Bitties/assets/TugOWar/ropeHolder.png", import.meta.url).href
+    );
+    this.load.image(
+      "lose",
+      new URL("../8Bitties/assets/TugOWar/fail_text.png", import.meta.url).href
+    );
+    this.load.image(
+      "meter",
+      new URL("../8Bitties/assets/TugOWar/ropeHolder.png", import.meta.url).href
+    );
+    this.load.image(
+      "dash",
+      new URL("../8Bitties/assets/TugOWar/imageholder.png", import.meta.url)
+        .href
     );
   }
 
@@ -62,21 +86,22 @@ export default class TugOWar extends Phaser.Scene {
     this.spacebar = this.input.keyboard.addKey(
       Phaser.Input.Keyboard.KeyCodes.SPACE
     );
-
-    this.dash = this.physics.add.image(75, 540, "player");
-    this.greenzone = this.physics.add.image(75, 360, "player");
-    this.player = this.add.sprite(300, 560, "player");
-    this.npc = this.add.sprite(780, 560, "npc");
+    this.graphics = this.make.graphics();
+    this.graphics.fillRect(290, 40, 500, 60);
+    this.mask = new Phaser.Display.Masks.GeometryMask(this, this.graphics);
+    this.progressBar.setMask(this.mask);
+    this.dash = this.physics.add.image(75, 540, "dash");
+    this.greenzone = this.physics.add
+      .image(75, 360, "greenzone")
+      .setVisible(false);
+    this.player = this.add.sprite(270, 560, "player").setDepth(2);
+    this.npc = this.add.image(810, 560, "npc").setDepth(2);
+    this.npc.flipX = true;
   }
 
   update() {
-    console.log(this.progNum);
-    if (424 > this.dash.y > 296) {
-      this.moveProgress();
-    } else {
-      this.progressBar.x -= 1;
-      this.progNum -= 1;
-    }
+    console.log(this.progNum, "barvalue");
+    this.winOrLose();
     this.scalePull();
     this.startDashMovement();
   }
@@ -97,24 +122,65 @@ export default class TugOWar extends Phaser.Scene {
       if (this.dash.y <= 552) {
         this.dash.y += 2;
       }
-      if (Phaser.Input.Keyboard.JustDown(this.spacebar)) {
+      if (
+        Phaser.Input.Keyboard.JustDown(this.spacebar) &
+        (this.imageCreated === false)
+      ) {
         this.dash.y -= 35;
+      }
+      if (this.dash.y > 264 && this.dash.y < 424) {
+        this.moveProgress();
       }
     }
   }
 
   gameStart() {
-    this.rope = this.add.sprite(540, 460, "rope");
-    this.meter = this.add.image(75, 360, "rope");
+    this.rope = this.add.sprite(540, 580, "rope").setDepth(1);
+    this.meter = this.add.image(75, 360, "meter");
     this.gamestarted = true;
   }
   moveProgress() {
-    if (this.progressBar.x < 690) {
-      this.progressBar.x += 2;
-      this.progNum += 2;
+    if (this.progressBar.x < 770) {
+      this.progressBar.x += 5;
+      this.progNum += 5;
     }
-    if (this.progNum >= 150) {
-      console.log("you: W");
+  }
+  winOrLose() {
+    if (this.progNum >= 230) {
+      this.youWin();
+    }
+    if (this.progNum <= -220) {
+      this.youLose();
+    }
+    if (
+      this.progressBar.x > 250 &&
+      this.gamestarted & (this.imageCreated === false)
+    ) {
+      this.progressBar.x -= 2;
+      this.progNum -= 2;
+    }
+  }
+
+  youLose() {
+    if (this.imageCreated === false) {
+      this.youlose = this.add.image(540, 360, "lose").setDepth(100);
+      this.imageCreated = true;
+    }
+    if (this.loseScale <= 1) {
+      this.LoseTimer += 1;
+      this.loseScale += 0.2 / this.LoseTimer;
+      this.youlose.setScale(this.loseScale);
+    }
+  }
+  youWin() {
+    if (this.imageCreated === false) {
+      this.youwin = this.add.image(540, 360, "win").setDepth(100);
+      this.imageCreated = true;
+    }
+    if (this.winScale <= 1) {
+      this.winTimer += 1;
+      this.winScale += 0.2 / this.winTimer;
+      this.youwin.setScale(this.winScale);
     }
   }
 }
