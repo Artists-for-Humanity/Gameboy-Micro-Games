7df64f93cf69a122ba60fcd89a96f57564400623
Ctech 21K <ctech21k@Ctech-21Ks-iMac.local>
2022-07-22 11:31:21 -0400

    added new assets, new cursor assets, added new instruction assets. Code for shuffling equations.

--- src/scripts/TeamInflation/MicroGame31.js
+++ src/scripts/TeamInflation/MicroGame31.js
@@ -10,94 +10,86 @@ export default class MicroGame31 extends Phaser.Scene {
 
         // Game Object Declarations
         this.myText;
-        this.buttons = []
-        this.selectedButtonIndex = 0
-
-
+        this.buttons = [];
+        this.selectedButtonIndex = 0;
+        // TODO swap buttonValues order
+        this.buttonValues = [2, 4, 12, 16, 24, /*28, 49, 60, 62, 64*/];
+        // IMPORTANT! Make sure that buttonPosition, selectedValues, and buttons (after addButtons) have the same length!
+        this.buttonPositions = Phaser.Utils.Array.Shuffle([[280,620], [280, 360], [780,360], [780,620], [540, 469]])
+        this.selectedValues = [];
+        this.gameOver = false;
+        this.answerSheet = ['Button09', 'Button08', 'Button07', 'Button06', 'Button05'];
+        //this.selectedValues = Phaser.Utils.Array.Shuffle(this.selectedValues)
     }
 
     preload() {
         this.load.image("background", new URL("./assets/NGbackground.png", import.meta.url).href);
+        this.load.image("Button00", new URL("./assets/Button00.png", import.meta.url).href);
         this.load.image("Button01", new URL("./assets/Button01.png", import.meta.url).href);
         this.load.image("Button02", new URL("./assets/Button02.png", import.meta.url).href);
         this.load.image("Button03", new URL("./assets/Button03.png", import.meta.url).href);
         this.load.image("Button04", new URL("./assets/Button04.png", import.meta.url).href);
         this.load.image("Button05", new URL("./assets/Button05.png", import.meta.url).href);
+        this.load.image("Button06", new URL("./assets/Button06.png", import.meta.url).href);
+        this.load.image("Button07", new URL("./assets/Button07.png", import.meta.url).href);
+        this.load.image("Button08", new URL("./assets/Button08.png", import.meta.url).href);
+        this.load.image("Button09", new URL("./assets/Button09.png", import.meta.url).href);
+
         this.load.image("cursorimage", new URL("./assets/cursorimage.png", import.meta.url).href);
         this.load.image("Instructions", new URL("./assets/Instructionsim.png", import.meta.url).href);
 
     }
 
     create() {
-
+        
         this.cursors = this.input.keyboard.createCursorKeys();
 
-        const gameWidth = this.game.config.width;
-        const gameHeight = this.game.config.height;
-        // const { width, height } = this.scale
-        // const playButton = this.add.image(gameWidth * -910, gameHeight * -365, "Button01")
-		// .setDisplaySize(150, 50)   
-        this.add.image(gameWidth / 2, gameHeight / 2, "background");
-        this.equation1 = this.add.image(gameWidth - 540, gameHeight - 260, "Button01").setScale(0.68, 0.5);
-        this.equation1.setInteractive();
-        this.equation2 = this.add.image(gameWidth - 800, gameHeight - 100, "Button02").setScale(0.68, 0.5);
-        this.equation2.setInteractive();
-        this.equation3 = this.add.image(gameWidth - 800, gameHeight - 360, "Button03").setScale(0.68, 0.5);
-        this.equation3.setInteractive();
-        this.equation4 = this.add.image(gameWidth - 300, gameHeight - 360, "Button04").setScale(0.68, 0.5);
-        this.equation4.setInteractive();
-        this.equation5 = this.add.image(gameWidth - 300, gameHeight - 100, "Button05").setScale(0.68, 0.5);
-        this.equation5.setInteractive();
-        this.image = this.add.image(gameWidth - 540, gameHeight - 560, "Instructions").setScale(0.6, 0.5);
+        this.gameWidth = this.game.config.width;
+        this.gameHeight = this.game.config.height;
 
-        this.buttonSelector = this.add.image(0.68, 0.5, "cursorimage").setScale(0.03, 0.03);
+        this.add.image(this.gameWidth / 2, this.gameHeight / 2, "background");
         
-           
+        this.image = this.add.image(this.gameWidth - 540, this.gameHeight - 560, "Instructions").setScale(0.6, 0.5);
+   
 
-        this.buttons.push(this.equation1);
-        this.buttons.push(this.equation2);
-        this.buttons.push(this.equation3);
-        this.buttons.push(this.equation4);
-        this.buttons.push(this.equation5);
+        this.buttons.push(this.equation00);
+        this.buttons.push(this.equation01);
+        this.buttons.push(this.equation02);
+        this.buttons.push(this.equation03);
+        this.buttons.push(this.equation04);
 
-
-        this.selectButton(0)
+        this.addImgToButtons();
+        this.buttonSelector = this.add.image(0.68, 0.5, "cursorimage").setScale(0.07, 0.07);
+        this.userInput();
+        this.selectButton(0);
        
-        this.equation1.on('selected', () => {
-            console.log('8x3 was selected')
-        })   
-        this.equation2.on('selected', () => {
-            console.log('12 was selected')
-        })   
-        this.equation3.on('selected', () => {
-            console.log('56+6 was selected')
-        })   
-        this.equation4.on('selected', () => {
-            console.log('64 was selected')
-        })   
-        this.equation5.on('selected', () => {
-            console.log('4x4 was selected')
-        })   
-
         this.events.once(Phaser.Scenes.Events.SHUTDOWN, () => {
-            this.equation1.off('selected')
-            this.equation2.off('selected')
-            this.equation3.off('selected')
-            this.equation4.off('selected')
-            this.equation5.off('selected')
+            this.equation00.off('selected');
+            this.equation01.off('selected');
+            this.equation02.off('selected');
+            this.equation03.off('selected');
+            this.equation04.off('selected');
         })
+    }
+    
+    addImgToButtons(){
+
+        for (let i = 0; i < this.buttonPositions.length; i++) {
+            this.buttons[i] =  this.add.image(this.buttonPositions[i][0], this.buttonPositions[i][1], this.answerSheet[i]).setScale(0.68, 0.5);
+            this.buttons[i].setInteractive();
+        }
+        
     }
 
     selectButton(index)
 	{
-		// TODO
+		
         const currentButton = this.buttons[this.selectedButtonIndex]
         currentButton.setTint(0xffffff)
         const button = this.buttons[index]
         button.setTint(0x66ff7f)
         this.buttonSelector.x = button.x
         this.buttonSelector.y = button.y + 50
-        console.log(this.buttonSelector)
         this.selectedButtonIndex = index
 	}
 
@@ -112,19 +104,19 @@ export default class MicroGame31 extends Phaser.Scene {
         {
             index = this.buttons.length - 1
         }
-        this.selectButton(index)
+        this.selectButton(index);
 
     }
 
 	confirmSelection()
 	{
-		// TODO
+		
         const button = this.buttons[this.selectedButtonIndex]
         button.emit('selected')
 	}
 
     update() {
-        // 
+        
         const upJustPressed = Phaser.Input.Keyboard.JustDown(this.cursors.up)
 		const downJustPressed = Phaser.Input.Keyboard.JustDown(this.cursors.down)
 		const spaceJustPressed = Phaser.Input.Keyboard.JustDown(this.cursors.space)
@@ -142,8 +134,34 @@ export default class MicroGame31 extends Phaser.Scene {
 		{
 			this.confirmSelection()
 		}
-		
-		
+    }
+
+    checkList(){
+        console.log(this.selectedValues)
+        let result = true;
+        for (let i = 0; i < this.selectedValues.length; i++) {
+            if (this.selectedValues[i] != this.buttonValues[i]) {
+                result = false
+                break;
+            }
+        }
+        if (result) {
+            console.log("You won!")
+        }
+        else {
+            console.log("You lost!")
+        }
+    }
+
+    userInput() {
+
+        for (let i = 0; i < this.buttons.length; i++) {
+            this.buttons[i].on('selected', () => {
+                this.selectedValues.push(this.buttonValues[i]);
+                if (this.selectedValues.length === this.answerSheet.length) this.checkList();
+            }) 
+            
+        }
     }
 
     setText() {
@@ -157,6 +175,4 @@ export default class MicroGame31 extends Phaser.Scene {
     }
 }
 
-// create buttons objects for selection 
-// Add keyboard for user input
 // add math logic, for winning and losing conditions  
