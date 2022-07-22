c60646d464aa18e44bde2e97e0d025f50fb0621a
VillainMythic <87036074+VillainMythic@users.noreply.github.com>
2022-07-22 15:16:00 -0400

    added mud animations, fixed bug where the pointer went above the meter,

--- src/scripts/8Bitties/TugOWar.js
+++ src/scripts/8Bitties/TugOWar.js
@@ -42,11 +42,6 @@ export default class TugOWar extends Phaser.Scene {
         .href
     );
     this.load.image(
-      "mud",
-      new URL("../8Bitties/assets/TugOWar/imageholder.png", import.meta.url)
-        .href
-    );
-    this.load.image(
       "rope",
       new URL("../8Bitties/assets/TugOWar/rope.png", import.meta.url).href
     );
@@ -71,8 +66,22 @@ export default class TugOWar extends Phaser.Scene {
       new URL("../8Bitties/assets/TugOWar/playerFrames.png", import.meta.url)
         .href,
       {
-        frameWidth: 44,
-        frameHeight: 64,
+        frameWidth: 88,
+        frameHeight: 128,
+      }
+    );
+    this.load.image(
+      "bar",
+      new URL("../8Bitties/assets/TugOWar/progress_bar.png", import.meta.url)
+        .href
+    );
+    this.load.spritesheet(
+      "mud",
+      new URL("../8Bitties/assets/TugOWar/mudidlesprite.png", import.meta.url)
+        .href,
+      {
+        frameWidth: 663,
+        frameHeight: 237,
       }
     );
   }
@@ -81,8 +90,8 @@ export default class TugOWar extends Phaser.Scene {
     this.createAnimations();
     this.pull = this.add.image(540, 360, "pull").setDepth(1);
     this.grass = this.add.image(540, 360, "background").setDepth(-10);
-    this.mud = this.add.sprite(540, 560, "mud").setDepth(-9);
-    this.progressBar = this.add.image(540, 75, "rope");
+    this.mud = this.add.sprite(540, 620, "mud").setDepth(-9);
+    this.progressBar = this.add.image(540, 75, "bar");
     this.spacebar = this.input.keyboard.addKey(
       Phaser.Input.Keyboard.KeyCodes.SPACE
     );
@@ -91,7 +100,7 @@ export default class TugOWar extends Phaser.Scene {
     this.meter = this.add.image(75, 360, "meter");
     this.dash = this.physics.add.image(90, 540, "dash");
     this.graphics = this.make.graphics();
-    this.graphics.fillRect(290, 40, 500, 60);
+    this.graphics.fillRect(280, 45, 512, 60);
     this.mask = new Phaser.Display.Masks.GeometryMask(this, this.graphics);
     this.progressBar.setMask(this.mask);
     this.greenzone = this.physics.add
@@ -104,13 +113,12 @@ export default class TugOWar extends Phaser.Scene {
     this.npc = this.add.sprite(810, 560, "player").setDepth(2).play("pulling");
     this.npc.flipX = true;
   }
-
   update() {
+    console.log("prognum", this.progNum);
     this.winOrLose();
     this.scalePull();
     this.startDashMovement();
   }
-
   scalePull() {
     if (this.pullScale <= 1.5) {
       this.pullTimer += 1;
@@ -129,7 +137,8 @@ export default class TugOWar extends Phaser.Scene {
       }
       if (
         Phaser.Input.Keyboard.JustDown(this.spacebar) &
-        (this.imageCreated === false)
+        (this.imageCreated === false) &
+        (this.dash.y > 160)
       ) {
         this.dash.y -= 35;
       }
@@ -141,18 +150,19 @@ export default class TugOWar extends Phaser.Scene {
 
   gameStart() {
     this.gamestarted = true;
+    this.mud.anims.play("mud");
   }
   moveProgress() {
-    if (this.progressBar.x < 770) {
+    if (this.progressBar.x < 790) {
       this.progressBar.x += 5;
       this.progNum += 5;
     }
   }
   winOrLose() {
-    if (this.progNum >= 230) {
+    if (this.progNum >= 250) {
       this.youWin();
     }
-    if (this.progNum <= -220) {
+    if (this.progNum <= -250) {
       this.youLose();
     }
     if (
@@ -163,9 +173,8 @@ export default class TugOWar extends Phaser.Scene {
       this.progNum -= 2;
     }
   }
-
   youLose() {
-    console.log("mudsplash animation here");
+    // console.log("mudsplash animation here");
     if (this.imageCreated === false) {
       this.youlose = this.add.image(540, 360, "lose").setDepth(100);
       this.imageCreated = true;
@@ -191,16 +200,35 @@ export default class TugOWar extends Phaser.Scene {
     this.anims.create({
       key: "pulling",
       frames: [
-        {
-          key: "player",
-          frame: 0,
-        },
-        {
-          key: "player",
-          frame: 1,
-        },
+        { key: "player", frame: 0 },
+        { key: "player", frame: 1 },
       ],
       frameRate: 1,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: "mud",
+      frames: [
+        { key: "mud", frame: 0 },
+        { key: "mud", frame: 1 },
+        { key: "mud", frame: 2 },
+        { key: "mud", frame: 2 },
+        { key: "mud", frame: 3 },
+        { key: "mud", frame: 4 },
+        { key: "mud", frame: 5 },
+        { key: "mud", frame: 6 },
+        { key: "mud", frame: 7 },
+        { key: "mud", frame: 8 },
+        { key: "mud", frame: 9 },
+        { key: "mud", frame: 10 },
+        { key: "mud", frame: 11 },
+        { key: "mud", frame: 12 },
+        { key: "mud", frame: 13 },
+        { key: "mud", frame: 14 },
+        { key: "mud", frame: 15 },
+      ],
+      frameRate: 10,
       repeat: -1,
     });
   }
