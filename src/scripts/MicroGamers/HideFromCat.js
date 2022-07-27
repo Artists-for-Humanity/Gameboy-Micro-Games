b9464fb93250ffe001ef30d0ce0fe7e5505b8176
gordanliu <gordonliu2004@gmail.com>
2022-07-27 17:58:32 -0400

    replaced assets, added cheese in, added win/lose conditions

--- src/scripts/MicroGamers/HideFromCat.js
+++ src/scripts/MicroGamers/HideFromCat.js
@@ -18,6 +18,7 @@ export default class MicroGame22 extends Phaser.Scene {
         this.laser;
         this.hitbox1;
         this.hitbox2;
+        this.hitboxes = [];
         this.mouse;
         
         this.Left;
@@ -25,7 +26,14 @@ export default class MicroGame22 extends Phaser.Scene {
 
         this.side = 0;
         this.sweeping = false;
+        this.touched = false;
+        this.textDisplayed = false;
         this.gameover = false;
+        this.gamestarted = false;
+
+        this.startText;
+        this.deadText;
+        this.winText;
     }
 
     preload() {
@@ -45,8 +53,14 @@ export default class MicroGame22 extends Phaser.Scene {
             import.meta.url).href);
         this.load.image('hitbox', new URL('assets/HideFromCat/hitbox.png',
             import.meta.url).href);
-        this.load.image('mouse', new URL('assets/HideFromCat/mouse.png',
-            import.meta.url).href);
+        this.load.spritesheet(
+            'mouse',
+            new URL('assets/HideFromCat/mouse.png', import.meta.url).href,
+            {
+                frameWidth: 241,
+                frameHeight: 180,
+            }
+            );
         this.load.spritesheet(
             'laser',
             new URL('assets/HideFromCat/laser.png', import.meta.url).href,
@@ -62,35 +76,55 @@ export default class MicroGame22 extends Phaser.Scene {
         this.cat = this.add.image(540, 360, 'cat');
         this.eyes = this.add.image(540, 360, 'eyes');
         this.table = this.add.image(540, 360, 'table');
-        this.cup = this.add.image(500, 360, 'cup');
+        this.cup = this.add.image(500, 370, 'cup');
         this.cup.depth = 1;
         this.plates = this.add.image(590, 360, 'plates');
         this.plates.depth = 1;
+        this.cheese = this.physics.add.sprite(910, 665, 'cheese');
+        this.cheese.setScale(0.6);
+        this.cheese.depth = 3;
         this.laser = this.physics.add.sprite(540, 360, 'laser');
         this.hitbox1 = this.physics.add.sprite(0, 0, 'hitbox');
-        // this.hitbox1.alpha = 0;
+        this.hitbox1.alpha = 0;
         this.hitbox2 = this.physics.add.sprite(0, 0, 'hitbox');
-        // this.hitbox2.alpha = 0;
+        this.hitbox2.alpha = 0;
+        this.hitboxes = [this.hitbox1, this.hitbox2];
         this.mouse = this.physics.add.sprite(150, 660, 'mouse');
         this.mouse.setScale(0.4);
         this.mouse.flipX = true;
         this.mouse.depth = 2;
         this.createAnims();
 
+        this.physics.add.collider(this.mouse, this.hitboxes, () => { //collision detection between mouse and laser hitboxes
+            this.lose();
+        });
+        this.physics.add.collider(this.mouse, this.cheese, () => { //collision detection between mouse and cheese
+            this.touched = true;
+            this.cheese.y = 620;
+        });
+
         this.Left = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
         this.Right = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
     }
 
     update() {
-        if (this.Right.isDown) {
-            this.mouse.flipX = true;
-            this.mouse.x += 8;
+        if (!this.textDisplayed) {
+            this.textDisplayed = true;
+            this.displayStartText();
+            this.time.delayedCall(3000, () => { //delete text and start game after 3 seconds
+                this.startText.alpha = 0; 
+                this.gamestarted = true; 
+            }, [], this); 
         }
-        if (this.Left.isDown) {
-            this.mouse.flipX = false;
-            this.mouse.x -= 8;
+        if (!this.gameover && this.gamestarted) {
+            this.startSweeping();
+            this.updatePlayer();
+            if (this.touched) {
+                if (this.mouse.flipX) this.cheese.x = this.mouse.x + 10;
+                if (!this.mouse.flipX) this.cheese.x = this.mouse.x - 10;
+                if (this.mouse.x <= 360) this.win();
+            }
         }
-        this.startSweeping();
     }
 
     createAnims() {
@@ -129,6 +163,38 @@ export default class MicroGame22 extends Phaser.Scene {
             frames: [{ key: 'laser', frame: 6 }],
             frameRate: 10
         });
+
+        this.anims.create({
+            key: 'idle',
+            frames: [{ key: 'mouse', frame: 0 }],
+            frameRate: 10
+        });
+        this.anims.create({
+            key: 'run',
+            frames: [
+            { key: 'mouse', frame: 0 },
+            { key: 'mouse', frame: 1 },
+            { key: 'mouse', frame: 2 },] ,
+            frameRate: 12
+        });
+    }
+
+    displayStartText() {
+        this.startText = this.add.text(540, 160, '')
+        this.startText.setStyle({
+            fontSize: '72px',
+            fill: '#00ffff',
+            align: 'center',
+            stroke: '#808080',
+            strokeThickness: 8
+        });
+        this.startText.setText([
+            "Steal the ",
+            "cheese without",
+            "getting spotted!"]);
+        this.startText.setOrigin(0.5);
+        this.startText.depth = 20;
+        this.startText.alpha = 1;
     }
 
     sweepRight() {
@@ -138,7 +204,7 @@ export default class MicroGame22 extends Phaser.Scene {
         }, [], this);
         this.time.delayedCall(300, () => {
             this.laser.anims.play('2');
-            this.hitbox2.setPosition(530, 670);
+            this.hitbox2.setPosition(570, 670);
         }, [], this);
         this.time.delayedCall(450, () => {
             this.laser.anims.play('3');
@@ -150,7 +216,7 @@ export default class MicroGame22 extends Phaser.Scene {
         }, [], this);
         this.time.delayedCall(750, () => {
             this.laser.anims.play('5');
-            this.hitbox1.setPosition(525, 670);
+            this.hitbox1.setPosition(570, 670);
             this.hitbox2.setPosition(0);
         }, [], this);
         this.time.delayedCall(900, () => {
@@ -164,7 +230,7 @@ export default class MicroGame22 extends Phaser.Scene {
         this.laser.anims.play('6');
         this.time.delayedCall(150, () => {
             this.laser.anims.play('5');
-            this.hitbox1.x = 525;
+            this.hitbox1.x = 570;
         }, [], this);
         this.time.delayedCall(300, () => {
             this.laser.anims.play('4');
@@ -177,7 +243,7 @@ export default class MicroGame22 extends Phaser.Scene {
         }, [], this);
         this.time.delayedCall(600, () => {
             this.laser.anims.play('2');
-            this.hitbox2.x = 530;
+            this.hitbox2.x = 570;
         }, [], this);
         this.time.delayedCall(750, () => {
             this.laser.anims.play('1');
@@ -188,24 +254,74 @@ export default class MicroGame22 extends Phaser.Scene {
         }, [], this);
     }
 
-    startSweeping() {
+    startSweeping() { //this.side used to prevent multiple calls of sweepRight and sweepLeft
         if (this.laser.frame.name === 0 && this.side === 0) {
             this.side = 1;
-            this.time.delayedCall(2200, this.sweepRight, [], this);
+            this.time.delayedCall(1800, this.sweepRight, [], this);
         }
         if (this.laser.frame.name === 6 && this.side === 1) {
             this.side = 0;
-            this.time.delayedCall(2200, this.sweepLeft, [], this);
+            this.time.delayedCall(1800, this.sweepLeft, [], this);
         }
     }
 
-    setText() {
-        this.myText = this.add.text(400, 360, '')
-        this.myText.setStyle({
+    updatePlayer() {
+        if (this.Right.isDown) {
+            this.mouse.flipX = true;
+            this.mouse.anims.play('run', true);
+            this.mouse.x += 8;
+        }
+        else if (this.Left.isDown) {
+            this.mouse.flipX = false;
+            this.mouse.anims.play('run', true);
+            this.mouse.x -= 8;
+        }
+        else this.mouse.anims.play('idle');
+    }
+
+    lose() {
+        if (!this.gameover) {
+            this.time.removeAllEvents();
+            this.displayDeadText();
+            this.gameover = true;
+        }
+    }
+
+    displayDeadText() {
+        this.deadText = this.add.text(540, 360, '')
+        this.deadText.setStyle({
             fontSize: '100px',
-            fill: '#000000',
+            fill: '#ff0000',
             align: 'center',
+            stroke: '#808080',
+            strokeThickness: 8
         });
-        this.myText.setText('Team4');
+        this.deadText.setText(
+            ["You were",
+            "spotted!"]);
+        this.deadText.setOrigin(0.5);
+        this.deadText.depth = 20;
+    }
+
+    win() {
+        if (!this.gameover) {
+            this.time.removeAllEvents();
+            this.displayWinText();
+            this.gameover = true;
+        }
+    }
+
+    displayWinText() {
+        this.winText = this.add.text(540, 360, '')
+        this.winText.setStyle({
+            fontSize: '100px',
+            fill: '#00ff00',
+            align: 'center',
+            stroke: '#808080',
+            strokeThickness: 8
+        });
+        this.winText.setText("You won!");
+        this.winText.setOrigin(0.5);
+        this.winText.depth = 20;
     }
 }
