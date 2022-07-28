071c05faa355e84455e97edefa94456e37920928
Ctech 21K <ctech21k@Ctech-21Ks-iMac.local>
2022-07-28 16:40:05 -0400

    Create BetweenSpace.js

---
+++ src/scripts/TeamInflation/BetweenSpace.js
@@ -0,0 +1,19 @@
+export default class BewteenSpace extends Phaser.Scene {
+    
+
+    constructor() {
+        super({
+            active: false,
+            visible: false,
+            key: 'BewteenSpace',
+        });
+
+
+}   }
+
+preload(){}
+
+
+create() {}
+
+update() {}
