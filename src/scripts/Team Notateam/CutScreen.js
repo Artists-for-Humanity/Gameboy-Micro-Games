842ab0fc24640a41d61551d9ada0dc42bd314b40
Trevor Ward <56857431+MassterKek@users.noreply.github.com>
2022-08-09 18:02:09 -0400

    Finished animations, cleaned up code
    Looks good now, ready for use

--- src/scripts/Team Notateam/CutScreen.js
+++ src/scripts/Team Notateam/CutScreen.js
@@ -1,11 +1,8 @@
 import Phaser from 'phaser';
 const X = 1080
 const Y = 720
-
 const L_END = X/4
 const L_START = -L_END
-
-
 const R_START = 5*L_END
 const R_END = 3*L_END
 export default class CutScreen extends Phaser.Scene {
@@ -27,16 +24,11 @@ export default class CutScreen extends Phaser.Scene {
         this.closed = false
         this.open = false
 
-        this.lost
-        this.score = 0
+        this.lost = false
+        this.score = 9
 
         this.space
 
-        this.heart
-        this.gba
-        this.life_group
-        this.socket
-        this.socket_group
         this.l_door
         this.r_door
 
@@ -50,10 +42,6 @@ export default class CutScreen extends Phaser.Scene {
         this.r_sockets
         this.l_life
         this.r_life
-        
-        this.fly
-        this.timer = 0
-
     }
 
     preload() {
@@ -62,6 +50,9 @@ export default class CutScreen extends Phaser.Scene {
             {   frameWidth: 252, frameHeight: 162})
         this.load.spritesheet(
             'loss', new URL('assets/loss_cat.png', import.meta.url).href,
+            {   frameWidth: 419, frameHeight: 162})
+        this.load.spritesheet(
+            'win', new URL('assets/win_cat.png', import.meta.url).href,
             {   frameWidth: 419, frameHeight: 162})
         this.load.spritesheet(
             'numbers', new URL('assets/numsheet.png', import.meta.url).href,
@@ -73,101 +64,12 @@ export default class CutScreen extends Phaser.Scene {
     }
 
     create() {
-
-        // Add images to Scene
-        this.l_door = this.add.image(L_START, Y/2, 'l_door')
-        this.r_door = this.add.image(R_START, Y/2, 'r_door')
-
-        this.faceplate = this.physics.add.sprite(R_START, Y/4, 'loss')
-
-        this.numplate = this.physics.add.sprite(L_START, Y/4, 'num_plate')
-        this.numplate.setScale(2/3, 1)
-        this.ones = this.physics.add.sprite(L_START + 82, Y/4, 'numbers')
-        this.tens = this.physics.add.sprite(L_START, Y/4, 'numbers')
-        this.huns = this.physics.add.sprite(L_START - 82, Y/4, 'numbers')
-
-        //TO BE CONTINUED
-        this.l_sockets = this.physics.add.group({
-            key: 'gba_socket',
-            repeat: 1,
-            setXY: { x:L_START, y: 5*Y/6 - 185, stepY: 185 }
-        })
-
-        this.l_life = this.physics.add.group({
-            key: 'gba_socket',
-            repeat: 1,
-            setXY: { x:L_START, y: 5*Y/6 - 185, stepY: 185 }
-        })
-
-        this.r_sockets = this.physics.add.group({
-            key: 'gba_socket',
-            repeat: 1,
-            setXY: { x:R_START, y: 5*Y/6 - 185, stepY: 185 }
-        })
-
-        this.r_life = this.physics.add.group({
-            key: 'gba_socket',
-            repeat: 1,
-            setXY: { x:R_START, y: 5*Y/6 - 185, stepY: 185 }
-        })
-
-        this.l_sockets.setDepth(1)
-        this.r_sockets.setDepth(1)
-        this.l_life.setDepth(1)
-        this.r_life.setDepth(1)
-
-        this.anims.create({
-            key: 'life',
-            frames: [
-                { key: 'gba', frame: 0 },
-                { key: 'gba', frame: 0 },
-                { key: 'gba', frame: 1 },
-                { key: 'gba', frame: 2 },
-                { key: 'gba', frame: 2 },
-                { key: 'gba', frame: 1 }
-            ],
-            frameRate: 6,
-            repeat: -1
-        })    
-
-        this.anims.create({
-            key: 'blink',
-            frames: [
-                { key: 'gba', frame: 0 },
-                { key: 'gba_socket'}
-            ],
-            frameRate: 6,
-            repeat: 2
-        })
-        
-        this.anims.create({
-            key:'cry',
-            frames: [
-                { key: 'loss', frame: 0 },
-                { key: 'loss', frame: 1},
-                { key: 'loss', frame: 2},
-                { key: 'loss', frame: 3}
-            ],
-            frameRate: 6,
-            repeat: -1,
-            yoyo:true
-        })
-
-        this.setScore(this.score)
-
         this.space = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP)
 
-        this.l_life.children.iterate((child) => {
-            child.anims.play('life')
-        });
-        this.r_life.children.iterate((child) => {
-            child.anims.play('life')
-        });
-
-        this.lost = true
-        this.score = 23
+        this.buildAnimations()
+        this.buildObjects()
+        this.setScore(this.score)
     }
-
     update() {
         if(!this.closed){
             this.close_timer++
@@ -177,12 +79,10 @@ export default class CutScreen extends Phaser.Scene {
             this.close_timer++
             this.open_doors()
         }
-
         if(Phaser.Input.Keyboard.JustDown(this.space)){
             this.open = true
         }
     }
-
 
     close_doors(){
         // If left door is not yet in closed position
@@ -197,7 +97,6 @@ export default class CutScreen extends Phaser.Scene {
             this.closecon()
         }
     }
-
     open_doors(){
         if(this.l_door.x > L_START){
             this.l_open()
@@ -210,7 +109,6 @@ export default class CutScreen extends Phaser.Scene {
             this.closed = false
         }
     }
-
     l_close(){
         // If left door would overshoot closed position
         if(this.l_door.x + this.close_timer >= L_END){
@@ -249,7 +147,6 @@ export default class CutScreen extends Phaser.Scene {
             });
         }
     }
-
     l_open(){
         // code for right door based on code for left door
         if(this.l_door.x - this.close_timer <= L_START){
@@ -291,7 +188,6 @@ export default class CutScreen extends Phaser.Scene {
 
         }
     }
-
     r_close(){
         // code for right door based on code for left door
         if(this.r_door.x - this.close_timer <= R_END){
@@ -321,7 +217,6 @@ export default class CutScreen extends Phaser.Scene {
             });
         }
     }
-
     r_open(){
         // If left door would overshoot closed position
         if(this.r_door.x + this.close_timer >= R_START){
@@ -351,7 +246,6 @@ export default class CutScreen extends Phaser.Scene {
             });
         }
     }
-
     reduce_life(){
         switch(this.life_total){
             case 1: case 2: this.r_disable(this.life_total); break;
@@ -359,38 +253,152 @@ export default class CutScreen extends Phaser.Scene {
             default: return;
         }
     }
-
     l_disable(index){
         this.l_life.getChildren()[4-index].anims.play('blink')
     }
-
     r_disable(index){
         this.r_life.getChildren()[2-index].anims.play('blink')
     }
-
     setScore(score){
         let o = score%10
         let h = Math.floor(score/100)
         let t = Math.floor((score-(h*100))/10)
-
-        
         this.ones.setFrame(o + 1)
         if(score >= 10)
             this.tens.setFrame(t + 1)
         if(score >= 100)
             this.huns.setFrame(h + 1)
     }
-
     closecon(){
         if(!this.lost){
+            this.faceplate.anims.play('win1').once('animationcomplete', () => {
+                this.faceplate.anims.play('win2')
+            })
             this.score++
-            this.setScore(this.score)
+            setTimeout(()=>{
+                this.setScore(this.score)
+            }, 200)
+            
         }
         else{
-            this.faceplate.anims.play('cry')
+            this.faceplate.anims.play('lose1').once('animationcomplete', () => {
+                this.faceplate.anims.play('lose2')
+            })
             this.life_total--
             this.reduce_life()
         }
     }
-    
+    buildObjects(){
+        // Build Doors
+        this.l_door = this.add.image(L_START, Y/2, 'l_door')
+        this.r_door = this.add.image(R_START, Y/2, 'r_door')
+        this.buildFaceplates()
+        this.buildLifeSockets()
+    }
+    buildFaceplates(){
+        this.faceplate = this.physics.add.sprite(R_START, Y/4, 'loss')
+        this.numplate = this.physics.add.sprite(L_START, Y/4, 'num_plate')
+        this.numplate.setScale(2/3, 1)
+        this.ones = this.physics.add.sprite(L_START + 82, Y/4, 'numbers')
+        this.tens = this.physics.add.sprite(L_START, Y/4, 'numbers')
+        this.huns = this.physics.add.sprite(L_START - 82, Y/4, 'numbers')
+    }
+    buildLifeSockets(){
+        this.l_sockets = this.physics.add.group({
+            key: 'gba_socket',
+            repeat: 1,
+            setXY: { x:L_START, y: 5*Y/6 - 185, stepY: 185 }
+        })
+        this.l_life = this.physics.add.group({
+            key: 'gba_socket',
+            repeat: 1,
+            setXY: { x:L_START, y: 5*Y/6 - 185, stepY: 185 }
+        })
+        this.r_sockets = this.physics.add.group({
+            key: 'gba_socket',
+            repeat: 1,
+            setXY: { x:R_START, y: 5*Y/6 - 185, stepY: 185 }
+        })
+        this.r_life = this.physics.add.group({
+            key: 'gba_socket',
+            repeat: 1,
+            setXY: { x:R_START, y: 5*Y/6 - 185, stepY: 185 }
+        })
+        this.l_sockets.setDepth(1)
+        this.r_sockets.setDepth(1)
+        this.l_life.setDepth(1)
+        this.r_life.setDepth(1)
+        this.l_life.children.iterate((child) => {
+            child.anims.play('life')
+        });
+        this.r_life.children.iterate((child) => {
+            child.anims.play('life')
+        });
+    }
+    buildAnimations(){
+        this.anims.create({
+            key: 'life',
+            frames: [
+                { key: 'gba', frame: 0 },
+                { key: 'gba', frame: 0 },
+                { key: 'gba', frame: 1 },
+                { key: 'gba', frame: 2 },
+                { key: 'gba', frame: 2 },
+                { key: 'gba', frame: 1 }
+            ],
+            frameRate: 6,
+            repeat: -1
+        })    
+        this.anims.create({
+            key: 'blink',
+            frames: [
+                { key: 'gba', frame: 0 },
+                { key: 'gba_socket'}
+            ],
+            frameRate: 6,
+            repeat: 2
+        })      
+        this.anims.create({
+            key:'lose1',
+            frames: [
+                { key: 'loss', frame: 0 },
+                { key: 'loss', frame: 1},
+                { key: 'loss', frame: 2},
+                { key: 'loss', frame: 3}
+            ],
+            frameRate: 6
+        })
+        this.anims.create({
+            key:'lose2',
+            frames: [
+                { key: 'loss', frame: 3 },
+                { key: 'loss', frame: 4},
+                { key: 'loss', frame: 5}
+            ],
+            frameRate: 6,
+            repeat: -1,
+            yoyo:true
+        })
+        this.anims.create({
+            key:'win1',
+            frames: [
+                { key: 'win', frame: 0 },
+                { key: 'win', frame: 1},
+                { key: 'win', frame: 2},
+                { key: 'win', frame: 3}
+            ],
+            frameRate: 6
+        })
+        this.anims.create({
+            key:'win2',
+            frames: [
+                { key: 'win', frame: 3},
+                { key: 'win', frame: 4},
+                { key: 'win', frame: 5}
+            ],
+            frameRate: 6,
+            repeat: -1,
+            yoyo:true
+        })
+    }
 }
