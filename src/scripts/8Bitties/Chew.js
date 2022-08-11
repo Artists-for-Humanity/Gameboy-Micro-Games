86ad5c047d86616f03966b9d302ea30d6d673d57
VillainMythic <87036074+VillainMythic@users.noreply.github.com>
2022-08-11 18:02:09 -0400

    finished most of marcy's eating code

--- src/scripts/8Bitties/Chew.js
+++ src/scripts/8Bitties/Chew.js
@@ -13,37 +13,52 @@ export default class Chew extends Phaser.Scene {
     this.biteCount = 0;
     this.chewInt = 0;
     this.marcy;
+    this.frameNum = 0;
   }
   preload() {
     this.load.spritesheet(
       "8B1_meat",
-      new URL("../8Bitties/assets/Chew/fly_sheet.png", import.meta.url).href,
+      new URL("../8Bitties/assets/Chew/marcy_meat.png", import.meta.url).href,
       {
-        frameHeight: 128,
-        frameWidth: 190,
+        frameHeight: 509,
+        frameWidth: 253,
       }
     );
     this.load.spritesheet(
       "8B1_marcy",
-      new URL("../8Bitties/assets/Chew/flyswatter_sheet.png", import.meta.url)
-        .href,
+      new URL("../8Bitties/assets/Chew/marcy.png", import.meta.url).href,
       {
-        frameHeight: 430,
-        frameWidth: 322,
+        frameHeight: 610,
+        frameWidth: 688,
       }
+    );
+    this.load.spritesheet(
+      "8B1_marcyTail",
+      new URL("../8Bitties/assets/Chew/marcy_tail.png", import.meta.url).href,
+      {
+        frameHeight: 610,
+        frameWidth: 688,
+      }
+    );
+    this.load.image(
+      "bg",
+      new URL("../8Bitties/assets/Chew/kitchenbg.png", import.meta.url).href
     );
   }
   create() {
+    this.bg = this.add.image(540, 360, "bg");
     this.spacebar = this.input.keyboard.addKey(
       Phaser.Input.Keyboard.KeyCodes.SPACE
     );
     this.makeAnims();
-    this.marcy = this.add.sprite(540, 360, "8B1_marcy");
-    this.drumStick = this.add.sprite(540, 400, "8B1_meat");
+    this.tail = this.add.sprite(340, 420, "8B1_marcyTail", 0);
+    this.marcy = this.add.sprite(340, 420, "8B1_marcy", this.frameNum);
+    this.drumStick = this.add.sprite(640, 400, "8B1_meat", 0);
+    this.tail.anims.play("wag", true);
   }
   update() {
-    this.meatStick();
-    this.chewing();
+    //this.meatStick();
+    if (this.biteCount < 6) this.chewing();
     console.log(this.biteCount);
     if (this.gameOver && !this.sent) {
       eventsCenter.emit("game-end", this.victory);
@@ -54,46 +69,27 @@ export default class Chew extends Phaser.Scene {
   }
   makeAnims() {
     this.anims.create({
-      key: "1",
-      frames: [{ key: "8B1_meat", frame: 0 }],
-      frameRate: 1,
-      repeat: 0,
+      key: "wag",
+      frames: [
+        { key: "8B1_marcyTail", frame: 0 },
+        { key: "8B1_marcyTail", frame: 1 },
+        { key: "8B1_marcyTail", frame: 2 },
+        { key: "8B1_marcyTail", frame: 3 },
+      ],
+      frameRate: 15,
+      repeat: -1,
     });
+
     this.anims.create({
-      key: "2",
-      frames: [{ key: "8B1_meat", frame: 1 }],
-      frameRate: 1,
-      repeat: 0,
-    });
-    this.anims.create({
-      key: "3",
-      frames: [{ key: "8B1_meat", frame: 2 }],
-      frameRate: 1,
-      repeat: 0,
-    });
-    this.anims.create({
-      key: "4",
-      frames: [{ key: "8B1_meat", frame: 3 }],
-      frameRate: 1,
-      repeat: 0,
-    });
-    this.anims.create({
-      key: "5",
-      frames: [{ key: "8B1_meat", frame: 4 }],
-      frameRate: 1,
-      repeat: 0,
-    });
-    this.anims.create({
-      key: "open",
-      frames: [{ key: "8B1_marcy", frame: 0 }],
-      frameRate: 1,
-      repeat: 0,
-    });
-    this.anims.create({
-      key: "closed",
-      frames: [{ key: "8B1_marcy", frame: 1 }],
-      frameRate: 1,
-      repeat: 0,
+      key: "lick",
+      frames: [
+        { key: "8B1_marcy", frame: 2 },
+        { key: "8B1_marcy", frame: 3 },
+        { key: "8B1_marcy", frame: 4 },
+      ],
+      frameRate: 6,
+      repeat: -1,
+      yoyo: true,
     });
   }
   chewing() {
@@ -101,23 +97,29 @@ export default class Chew extends Phaser.Scene {
     if (this.keyInt === 3) {
       this.chewInt += 1;
       this.keyInt = 0;
-      this.marcy.anims.play("open", true);
+      this.frameSwap();
     }
     if (this.chewInt === 2) {
       this.chewInt = 0;
       this.biteCount += 1;
-      this.marcy.anims.play("closed", true);
+      this.meatStick();
     }
   }
   meatStick() {
-    if (this.biteCount === 1) this.drumStick.anims.play("1");
-    if (this.biteCount === 2) this.drumStick.anims.play("2");
-    if (this.biteCount === 3) this.drumStick.anims.play("3");
-    if (this.biteCount === 4) this.drumStick.anims.play("4");
-    if (this.biteCount === 5) {
-      this.drumStick.anims.play("5");
+    this.drumStick.setFrame(this.biteCount);
+    if (this.biteCount === 6) {
+      this.marcy.play("lick");
       this.victory = true;
-      this.gameOver = true;
+      //this.gameOver = true;
     }
+  }
+  frameSwap() {
+    if (this.frameNum === 0) {
+      this.frameNum = 1;
+    } else {
+      this.frameNum = 0;
+    }
+
+    this.marcy.setFrame(this.frameNum);
   }
 }
