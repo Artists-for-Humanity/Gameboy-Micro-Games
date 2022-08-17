ec11e6f1e70c113587a9c3ffa08799598a327c60
HandyDorceus <dorceus.handy@gmail.com>
2022-08-17 16:12:13 -0400

    Squashed commit of the following:
    commit 9d906bf69f4e332e5d9d78c569b838fc0c79eb0d
    Author: HandyDorceus <dorceus.handy@gmail.com>
    Date:   Wed Aug 17 16:03:12 2022 -0400
    
        missing object error
    
    commit 7c8ccded99e3fd3ef6f27c94f7ef574035d7dd37
    Author: Ctech 21K <ctech21k@Ctech-21Ks-iMac.local>
    Date:   Wed Aug 17 15:45:54 2022 -0400
    
        finished circle game
    
    commit 43f49e1165d8c63784a42aa33caa05c83c4910e1
    Author: VillainMythic <87036074+VillainMythic@users.noreply.github.com>
    Date:   Wed Aug 17 11:34:52 2022 -0400
    
        Update CircleJump.js
    
    commit 453d5528aed24198bb108098403e96f3a3653ecd
    Author: VillainMythic <87036074+VillainMythic@users.noreply.github.com>
    Date:   Wed Aug 17 11:33:02 2022 -0400
    
        updated circle game to circle jump
    
    commit a900c530ddb0c56e1963897ce418f9f03f88301a
    Merge: 04c9e41 ed6c812
    Author: Ctech 21K <ctech21k@Ctech-21Ks-iMac.local>
    Date:   Tue Aug 16 17:08:49 2022 -0400
    
        Merge branch 'teaminflation' of https://github.com/Artists-for-Humanity/Gameboy-Micro-Games into teaminflation
    
    commit 04c9e4140884a2a844a19ad1c2da53da665345e7
    Author: Ctech 21K <ctech21k@Ctech-21Ks-iMac.local>
    Date:   Tue Aug 16 17:08:47 2022 -0400
    
        added new assets, code towards fixing circle game
    
        circle jump assets added
    
    commit ed6c8129b3d28c9f243e232df2fb85e3a21e1674
    Author: TunaYou <lolboyrl7@gmail.com>
    Date:   Tue Aug 16 15:50:34 2022 -0400
    
        assets done
    
    commit 0c9672e22b0a3ca75b96454bdf28b8a8c5ee5948
    Author: TunaYou <lolboyrl7@gmail.com>
    Date:   Tue Aug 16 12:11:46 2022 -0400
    
        final functionality changes betweenspace
    
    commit d4093c880565062afc0fce52ef0e93d4faa57ccb
    Author: Ctech 21K <ctech21k@Ctech-21Ks-iMac.local>
    Date:   Thu Aug 11 17:54:29 2022 -0400
    
        added assets for circle game, added animations
    
    commit 46bdf223ddbb1593259572e6715924dd3dd2d028
    Author: Ctech 21K <ctech21k@Ctech-21Ks-iMac.local>
    Date:   Wed Aug 10 18:00:27 2022 -0400
    
        change in game name
    
    commit 541197b1b44ce386ca8bae56cae8ed9bfa43da01
    Author: Ctech 21K <ctech21k@Ctech-21Ks-iMac.local>
    Date:   Wed Aug 10 17:53:04 2022 -0400
    
        fixed winning and losing assets, fixed platform placement
    
    commit 576702788cf23eb6dab64d96c813612c7331344f
    Author: Ctech 21K <ctech21k@Ctech-21Ks-iMac.local>
    Date:   Wed Aug 10 16:11:16 2022 -0400
    
        fixed flying animation
    
    commit bbc80832329ee3c928d7c123eba439876343b69b
    Merge: 52a7c6e d0d0815
    Author: TunaYou <lolboyrl7@gmail.com>
    Date:   Wed Aug 10 16:08:45 2022 -0400
    
        Merge branch 'teaminflation' of https://github.com/Artists-for-Humanity/Gameboy-Micro-Games into teaminflation
    
    commit 52a7c6e37c2ddf611896500abbb17c3b6fda6e5a
    Author: TunaYou <lolboyrl7@gmail.com>
    Date:   Wed Aug 10 16:08:35 2022 -0400
    
        changes to asteroid spawn
    
    commit d0d0815e8b6985ab5b6a0967024bee4a150be416
    Merge: a190ad4 3946c89
    Author: Ctech 21K <ctech21k@Ctech-21Ks-iMac.local>
    Date:   Wed Aug 10 15:40:51 2022 -0400
    
        Merge branch 'teaminflation' of https://github.com/Artists-for-Humanity/Gameboy-Micro-Games into teaminflation
    
    commit a190ad429366a4f3a579d7bdd36bb332ff0a703c
    Author: Ctech 21K <ctech21k@Ctech-21Ks-iMac.local>
    Date:   Wed Aug 10 15:39:52 2022 -0400
    
        created asset flies for number game,
    
    commit 3946c89eac90565a261e6ff51b8a89ee1b82a5d7
    Author: TunaYou <lolboyrl7@gmail.com>
    Date:   Wed Aug 10 15:39:38 2022 -0400
    
        victory gameOver variables
    
    commit 042ee060bb5081695b371c7a9f48a107a2cf6a45
    Author: TunaYou <lolboyrl7@gmail.com>
    Date:   Wed Aug 10 13:55:58 2022 -0400
    
        functionality spacegame complete

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,7 +8,6 @@ import TrashSort from "./DebugginDaOpps/TrashSort";
 import Cannon from "./DebugginDaOpps/Cannon";
 import Highest2Lowest from "./TeamInflation/Highest2Lowest";
 import FrogJump from "./TeamInflation/FrogJump";
-import CircleGame from "./TeamInflation/CircleGame";
 import BetweenSpace from "./TeamInflation/BetweenSpace";
 import ColorPasscode from "./MicroGamers/ColorPasscode";
 import HideFromCat from "./MicroGamers/HideFromCat";
@@ -20,14 +19,16 @@ import SockToss from "./Team Notateam/socktoss";
 import CutScreen from "./Team Notateam/CutScreen";
 import MainMenu from "./MainMenu";
 import Challenge from "./DebugginDaOpps/Challenge";
+import BetweenSpace from "./TeamInflation/BetweenSpace";
+import CircleJump from "./TeamInflation/CircleJump";
 
 // Set configuration for phaser game instance
 const config = {
   type: Phaser.AUTO,
   width: 1080,
   height: 720,
-  transparent: true,
-  // backgroundColor: '#4488aa',
+  transparent: false,
+  backgroundColor: "#4488aa",
 
   // Add physics, arcade, scene, and audio
   physics: {
@@ -42,7 +43,7 @@ const config = {
 
 
   scene: [
-    CutScreen, SockToss, CarPump, Emeowgency, BetweenSpace, CircleGame, FrogJump, Highest2Lowest, TugOWar, FlySwat, DrinkPour, Cannon, TrashSort, ColorPasscode, HideFromCat, HitTheButton, ColorLab
+    CutScreen, SockToss, CarPump, Emeowgency, BetweenSpace, CircleJump, FrogJump, Highest2Lowest, TugOWar, FlySwat, DrinkPour, Cannon, TrashSort, ColorPasscode, HideFromCat, HitTheButton, ColorLab
   ],
   audio: {
     disableWebAudio: true,
