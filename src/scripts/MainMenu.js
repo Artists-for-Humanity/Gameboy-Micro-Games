156bf62ceb838a5d07dd77e6e1a63aab7e4b3d0d
kyrie <echolocks@gmail.com>
2022-09-15 15:40:04 -0400

    removed xtra constructor from main

--- src/scripts/MainMenu.js
+++ src/scripts/MainMenu.js
@@ -1,7 +1,7 @@
-import eventsCenter from './EventsCenter'
+import eventsCenter from "./EventsCenter";
 
-const X = 1080
-const Y = 720
+const X = 1080;
+const Y = 720;
 
 export default class MainMenu extends Phaser.Scene {
   // Game  Class Constructor
@@ -11,148 +11,161 @@ export default class MainMenu extends Phaser.Scene {
       visible: false,
       key: "MainMenu",
     });
+
+    this.btns = [];
+    this.animations = [];
+    this.fingerIcon;
+    this.fingerPos = 0;
+
+    this.left;
+    this.right;
+    this.action;
+
+    this.sent = false;
+
+    this.wobbleDir = false;
+
+    this.wobbleTimer = 0;
   }
 
-    // Game  Class Constructor
-    constructor() {
-        super({
-            active: false,
-            visible: false,
-            key: 'MainMenu',
-        });
+  preload() {
+    this.listOfGames = [
+      "Emeowgency",
+      "ColorLab",
+      "MicroGame11",
+      "Highest2Lowest",
+      "FrogJump",
+      "CircleJump",
+      "BewteenSpace",
+      "ColorPasscode",
+      "HideFromCat",
+      "HitTheButton",
+      "TugOWar",
+      "FlySwat",
+      "DrinkPour",
+    ];
 
-        this.btns = [];
-        this.animations = [];
-        this.fingerIcon
-        this.fingerPos = 0
+    this.load.image(
+      "bg1",
+      new URL("globalAssets/title_screen.png", import.meta.url).href
+    );
+    this.load.spritesheet(
+      "play",
+      new URL("gameAssets/play_btn.png", import.meta.url).href,
+      { frameWidth: 239, frameHeight: 117 }
+    );
+    this.load.spritesheet(
+      "score",
+      new URL("gameAssets/score_btn.png", import.meta.url).href,
+      { frameWidth: 239, frameHeight: 117 }
+    );
+    this.load.image(
+      "finger",
+      new URL("gameAssets/finger.png", import.meta.url).href
+    );
+  }
 
-        this.left
-        this.right
-        this.action
+  create() {
+    this.add.image(X / 2, Y / 2, "bg1");
+    this.btns.push(this.physics.add.sprite(X / 8, Y * 0.9, "play"));
+    this.btns.push(this.physics.add.sprite((7 * X) / 8, Y * 0.9, "score"));
 
-        this.sent = false
+    this.fingerIcon = this.add
+      .image(this.btns[0].x, this.btns[0].y - 117, "finger")
+      .setRotation(Math.PI / 2);
 
-        this.wobbleDir = false
+    this.left = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+    this.right = this.input.keyboard.addKey(
+      Phaser.Input.Keyboard.KeyCodes.RIGHT
+    );
+    this.action = this.input.keyboard.addKey(
+      Phaser.Input.Keyboard.KeyCodes.SPACE
+    );
 
-        this.wobbleTimer = 0
+    this.animationBuilder();
+    this.updateSelection();
+  }
+
+  update() {
+    this.buttonPresses();
+    this.verticalWobble(this.fingerIcon, 0.5, 4);
+  }
+
+  buttonPresses() {
+    if (Phaser.Input.Keyboard.JustDown(this.left)) {
+      this.updateSelection("left");
+    }
+    if (Phaser.Input.Keyboard.JustDown(this.right)) {
+      this.updateSelection("right");
+    }
+    if (Phaser.Input.Keyboard.JustDown(this.action)) {
+      this.buttonEvents();
+    }
+  }
+
+  verticalWobble(obj, amount, speed) {
+    this.wobbleTimer += speed;
+    if (this.wobbleTimer % 100 === 0) {
+      this.wobbleDir = !this.wobbleDir;
+    }
+    this.wobbleDir ? (obj.y += amount) : (obj.y -= amount);
+  }
+
+  updateSelection(input) {
+    this.btns[this.fingerPos].anims.stop().setFrame(0);
+
+    if (input === "left") {
+      this.fingerPos === 0
+        ? (this.fingerPos = this.btns.length - 1)
+        : this.fingerPos--;
+    }
+    if (input === "right") {
+      this.fingerPos === this.btns.length - 1
+        ? (this.fingerPos = 0)
+        : this.fingerPos++;
     }
 
-    preload() {
-        this.listOfGames = [
-            "Emeowgency",
-            "ColorLab",
-            "MicroGame11",
-            "Highest2Lowest",
-            "FrogJump",
-            "CircleJump",
-            "BewteenSpace",
-            "ColorPasscode",
-            "HideFromCat",
-            "HitTheButton",
-            "TugOWar",
-            "FlySwat",
-            "DrinkPour"];
+    this.fingerIcon.x = this.btns[this.fingerPos].x;
+    this.btns[this.fingerPos].anims.play(this.animations[this.fingerPos]);
+  }
 
-        this.load.image('bg1', new URL('globalAssets/title_screen.png', import.meta.url).href);
-        this.load.spritesheet('play', new URL('gameAssets/play_btn.png', import.meta.url).href,
-            { frameWidth: 239, frameHeight: 117 })
-        this.load.spritesheet('score', new URL('gameAssets/score_btn.png', import.meta.url).href,
-            { frameWidth: 239, frameHeight: 117 })
-        this.load.image('finger', new URL('gameAssets/finger.png', import.meta.url).href);
+  buttonEvents() {
+    switch (this.fingerPos) {
+      case 0: // Play
+        this.playGame();
+        break;
+      case 1: // Scores
+      default:
+        break;
     }
+  }
 
-    create() {
-        this.add.image(X / 2, Y / 2, "bg1");
-        this.btns.push(this.physics.add.sprite(X / 8, Y * .90, 'play'))
-        this.btns.push(this.physics.add.sprite(7 * X / 8, Y * .90, 'score'))
-
-        this.fingerIcon = this.add.image(this.btns[0].x, this.btns[0].y - 117, 'finger').setRotation(Math.PI / 2)
-
-        this.left = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT)
-        this.right = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT)
-        this.action = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE)
-
-        this.animationBuilder()
-        this.updateSelection()
+  playGame() {
+    if (!this.sent) {
+      this.scene.run("CutScreen");
+      //this.globalState.sendMessage(true)
+      this.sent = true;
     }
+  }
 
-    update() {
-        this.buttonPresses()
-        this.verticalWobble(this.fingerIcon, .5, 4)
-    }
-
-    buttonPresses() {
-        if (Phaser.Input.Keyboard.JustDown(this.left)) {
-            this.updateSelection("left")
-        }
-        if (Phaser.Input.Keyboard.JustDown(this.right)) {
-            this.updateSelection("right")
-        }
-        if (Phaser.Input.Keyboard.JustDown(this.action)) {
-            this.buttonEvents()
-        }
-    }
-
-    verticalWobble(obj, amount, speed) {
-        this.wobbleTimer += speed
-        if (this.wobbleTimer % 100 === 0) {
-            this.wobbleDir = !this.wobbleDir
-        }
-        this.wobbleDir ? obj.y += amount : obj.y -= amount
-    }
-
-    updateSelection(input) {
-
-        this.btns[this.fingerPos].anims.stop().setFrame(0)
-
-        if (input === "left") {
-            this.fingerPos === 0 ? this.fingerPos = this.btns.length - 1 : this.fingerPos--
-        }
-        if (input === "right") {
-            this.fingerPos === this.btns.length - 1 ? this.fingerPos = 0 : this.fingerPos++
-        }
-
-        this.fingerIcon.x = this.btns[this.fingerPos].x
-        this.btns[this.fingerPos].anims.play(this.animations[this.fingerPos])
-    }
-
-    buttonEvents() {
-        switch (this.fingerPos) {
-            case 0: // Play
-                this.playGame()
-                break;
-            case 1: // Scores
-            default:
-                break;
-        }
-    }
-
-    playGame() {
-        if (!this.sent) {
-            this.scene.run("CutScreen")
-            //this.globalState.sendMessage(true)
-            this.sent = true
-        }
-    }
-
-    animationBuilder() {
-        this.animations.push(
-            this.anims.create({
-                key: 'play_btn',
-                frames: "play",
-                frameRate: 12,
-                repeat: -1,
-                yoyo: true
-            })
-        )
-        this.animations.push(
-            this.anims.create({
-                key: 'score_btn',
-                frames: "score",
-                frameRate: 12,
-                repeat: -1,
-                yoyo: true
-            })
-        )
-    }
+  animationBuilder() {
+    this.animations.push(
+      this.anims.create({
+        key: "play_btn",
+        frames: "play",
+        frameRate: 12,
+        repeat: -1,
+        yoyo: true,
+      })
+    );
+    this.animations.push(
+      this.anims.create({
+        key: "score_btn",
+        frames: "score",
+        frameRate: 12,
+        repeat: -1,
+        yoyo: true,
+      })
+    );
+  }
 }
