9cf5ce9edc542b31f3beb7066ee42a44e8fdd11c
Mindzeus <71579580+ph4iry@users.noreply.github.com>
2023-02-09 17:51:45 -0500

    added seals and bug fixes
    todo: create text

--- src/scripts/NewGames/PenguinSlide.js
+++ src/scripts/NewGames/PenguinSlide.js
@@ -19,7 +19,16 @@ export default class PenguinSlide extends Phaser.Scene {
     this.displayedPenguins = false;
     // custom properties
     this.penguinCount = Math.floor(Math.random() * 5) + 1;
-
+    this.sealCount = Math.floor(Math.random() * 4); // 0 to 3 penguins
+    this.animals = [...(new Array(this.penguinCount).fill('penguin')), ...(new Array(this.sealCount).fill('seal'))];
+    function shuffle(array) {
+      for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+      }
+    }
+    shuffle(this.animals);
+    this.animalTween = 0;
     this.buttonHandlers = new ButtonPressHandlers();
     this.gamePad = null;
   }
@@ -53,8 +62,8 @@ export default class PenguinSlide extends Phaser.Scene {
     this.gameWidth = this.game.config.width;
     this.gameHeight = this.game.config.height;
     this.add.image(this.gameWidth / 2, this.gameHeight / 2, "background").setOrigin(0.5);
-    this.add.image(0, 209, 'igloo').setOrigin(0).setDepth(1);
-    this.cursors = this.input.keyboard.createCursorKeys();
+    this.igloo = this.add.image(0, 209, 'igloo').setOrigin(0).setDepth(1);
+    // this.cursors = this.input.keyboard.createCursorKeys();
 
     this.createAnimations();
     this.currentGuess = 0;
@@ -91,15 +100,47 @@ export default class PenguinSlide extends Phaser.Scene {
     // this.winText.setVisible(false);
 
     eventsCenter.on('start_game', () => { this.started = true;});
-
     this.tweens.add({
       targets: this.path,
       delay: 5000,
       t: 1,
-      ease: 'Quart.easeIn',
+      ease: 'Sine.easeIn',
       duration: 1500,
       yoyo: false,
-      repeat: this.penguinCount - 1,
+      repeat: this.animals.length - 1,
+      onRepeat: () => {
+        this.penguin.setTexture(this.animals[this.animalTween]);
+        if (this.penguin.texture.key === 'seal') {
+          this.penguin.flipX = true;
+        } else {
+          this.penguin.flipX = false;
+        }
+        this.animalTween++;
+        console.log(this.animals);
+      },
+      onUpdate: () => {
+        if (this.penguin.texture.key === 'seal') {
+          if (this.penguin.anims.isPlaying) {
+            this.penguin.anims.stop();
+          }
+          if (this.path.t > 0.45) {
+            this.penguin.setAngle(0);
+          } else if (this.path.t > 0.65) {
+            this.penguin.setAngle(45)
+          }
+        } else if (this.penguin.texture.key === 'penguin') {
+          this.penguin.setAngle(0);
+          if (this.path.t < 0.4) {
+            this.penguin.anims.play('penguin-walk')
+          } else if (this.path.t > 0.45 && this.path.t < 0.48) {
+            this.penguin.anims.stopAfterRepeat();
+            this.penguin.setFrame(2);
+          } else if (this.path.t > 0.48) {
+            this.penguin.anims.stopAfterRepeat();
+            this.penguin.setFrame(3);
+          }
+        }
+      },
       onComplete: () => {
         this.displayedPenguins = true;
       }
@@ -114,6 +155,8 @@ export default class PenguinSlide extends Phaser.Scene {
 
     this.seal = this.add.sprite(100, 1500, 'seal', 0).setAngle(-25);
     this.seal.flipX = true;
+
+    this.zzz = this.add.sprite(this.igloo.getRightCenter().x, this.igloo.getTopRight().y + (this.igloo.height / 4), 'zzz', 0).setOrigin(0, 1).setVisible(false);
   }
 
   update() {
@@ -125,7 +168,10 @@ export default class PenguinSlide extends Phaser.Scene {
       if (this.displayedPenguins && !this.startTimer) {
         this.scorebox.anims.play('scorebox-shift');
         this.startTimer = true;
+        this.started = true;
         eventsCenter.emit('start_timer');
+      } else if (!this.displayedPenguins) {
+        eventsCenter.emit('reset_timer');
       }
       
       // this.displayPenguins(this.penguinCount);
@@ -164,8 +210,13 @@ export default class PenguinSlide extends Phaser.Scene {
   }
 
   updateGuess(num) {
+    // if the current guess is 0 and the num is also less than 0 do nothing
+    if (this.currentGuess === 0 && num < 0) {
+      return;
+    }
     this.currentGuess += num;
     this.scoreboxText.setText(this.currentGuess.toString());
+    // otherwise, update the current guess
   }
 
   confirmGuess() {
@@ -175,7 +226,7 @@ export default class PenguinSlide extends Phaser.Scene {
         targets: this.sealPath,
         t: 1,
         duration: 1500,
-        ease: 'Sine.easeInOut',
+        ease: 'Sine.easeOut',
         yoyo: false,
         repeat: 1,
         onComplete: () => {
@@ -186,7 +237,28 @@ export default class PenguinSlide extends Phaser.Scene {
         onUpdate: () => {
           this.sealCurve.getPoint(this.sealPath.t, this.sealPath.vec);
           this.seal.setPosition(this.sealPath.vec.x, this.sealPath.vec.y);
+
+          // this can be optimized a bit
+          if (this.sealPath.t < 0.25) {
+            this.seal.setAngle(-45);
+          } else if (this.sealPath.t < 0.5) {
+            this.seal.setAngle(-25);
+          } else if (this.sealPath.t > 0.4 && this.sealPath.t < 0.6) {
+            this.seal.setAngle(0);
+          } else if (this.sealPath.t > 0.5) {
+            this.seal.setAngle(25);
+          } else if (this.sealPath.t > 0.75) {
+            this.seal.setAngle(45)
+          }
         }
+      });
+    } else {
+      eventsCenter.emit('stop_timer');
+      this.penguin.setVisible(false);
+      this.zzz.setVisible(true);
+      this.zzz.anims.play('sleeping_igloo').once('animationcomplete', () => {
+        this.gameOver = true;
+        eventsCenter.emit('game_end');
       });
     }
   }
@@ -201,5 +273,84 @@ export default class PenguinSlide extends Phaser.Scene {
       frameRate: 2,
       repeat: -1,
     });
+    this.anims.create({
+      key: 'penguin-walk',
+      frames: [
+        { key: 'penguin', frame: 0 },
+        { key: 'penguin', frame: 1 },
+      ],
+      frameRate: 3,
+      repeat: -1,
+    });
+    this.anims.create({
+      key: 'penguin-slide',
+      frames: [
+        { key: 'penguin', frame: 2},
+        { key: 'penguin', frame: 3},
+      ],
+      repeat: 0,
+      frameRate: 2,
+    })
+    this.anims.create({
+      key: 'sleeping_igloo',
+      frames: [
+        { key: 'zzz', frame: 0 },
+        { key: 'zzz', frame: 1 },
+        { key: 'zzz', frame: 2 },
+        { key: 'zzz', frame: 3 },
+      ],
+      frameRate: 4,
+      repeat: 3,
+    });
+  }
+
+  setupText () {
+    // this.winText = this.add.text(540, 360, 'YOU WON!');
+    // this.winText.setStyle({
+    //     fontSize: '160px',
+    //     fill: '#00ff00',
+    //     align: 'center',
+    //     stroke: '#808080',
+    //     strokeThickness: 8
+    // });
+    // this.winText.setOrigin(0.5);
+    // this.winText.visible = false;
+    // this.winText.depth = 20;
+
+    // this.lossText = this.add.text(540, 360, 'YOU LOST!');
+    // this.lossText.setStyle({
+    //     fontSize: '160px',
+    //     fill: '#ff0000',
+    //     align: 'center',
+    //     stroke: '#808080',
+    //     strokeThickness: 8
+    // });
+    // this.lossText.setOrigin(0.5);
+    // this.lossText.visible = false;
+    // this.lossText.depth = 20;
+    this.look = this.add.text(540, 360, 'WATCH!', {
+      fontSize: '160px',
+      color: '#00ff00',
+      align: 'center',
+      // stroke: '#808080',
+      // strokeThickness: 8,
+    }).setVisible(false);
+    this.go = this.add.text(540, 360, 'GO!', {
+      fontSize: '160px',
+      color: '#00ff00',
+      align: 'center',
+    }).setVisible(false);
+
+    this.win = this.add.text(540, 360, 'YOU WIN!', {
+      fontSize: '160px',
+      color: '#00FF00',
+      align: 'center',
+    }).setVisible(false);
+    
+    this.lose = this.add.text(540, 360, 'YOU LOSE!', {
+      fontSize: '160px',
+      color: '#FF0000',
+      align: 'center'
+    }).setVisible(false);
   }
 }
