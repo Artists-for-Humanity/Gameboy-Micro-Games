4256af13a7c6dd6288eaaa80d36e4d4db616b451
VillainMythic <87036074+VillainMythic@users.noreply.github.com>
2023-03-28 17:57:42 -0400

    colotr;lab fix

--- src/scripts/8Bitties/ColorLab.js
+++ src/scripts/8Bitties/ColorLab.js
@@ -150,6 +150,23 @@ export default class ColorLab extends Phaser.Scene {
       this.globalState.timerMessage('start_timer');
     });
   }
+ColorLabReset(){
+  this.victory = false;
+  this.gameOver = false;
+  this.sent = false;
+  this.lose =false;
+  this.started = false;
+  this.catchScale = 0;
+  this.catchScale = 0;
+  this.safeScale = 0;
+  this.safeTimer = 0;
+  this.failScale = 0;
+  this.failTimer = 0;
+  this.createImage = false;
+  this.buttonHandlers = new ButtonPressHandlers();
+  this.gamePad = null;
+}
+
 
   update() {
     if (this.gameOver && !this.sent) {
