b7b034d6b7142f4e9ccf8b4d7416d5804c35e2df
Serenity <swilliams314159@gmail.com>
2023-07-11 17:07:50 -0400

    finished user input
    next up is a spritesheet for the thing so the user can see the input

--- src/scripts/Team Notateam/Yipee.js
+++ src/scripts/Team Notateam/Yipee.js
@@ -1,10 +1,8 @@
 import Phaser from "phaser";
 import ButtonPressHandlers from '../ButtonPressHandlers';
 import eventsCenter from "../EventsCenter";
-
 let l = 720;
 let w = 1080;
-
 export default class Yipee extends Phaser.Scene {
   constructor() {
     console.log("constructor start");
@@ -16,14 +14,6 @@ export default class Yipee extends Phaser.Scene {
       }
     );
     this.started = false;
-    //inputs
-    //i wrote down a bunch of stuff for input- so im going to make an array for the little analog limbs so the navigation is easier. the even numbers are the ones that are the buttons, the rest are not visible
-    //each analog limb is going to have some variables within to indicate whether they are highlighted, selected, or neither
-    //gotta figure out how to set up the analog limbs but that's another story i can figure out later
-    //what i set up so far was the input keys for the controller but i haven't gotten to testing them yet.
-    //6/21  make the analog limbs and set them up and test the input
-    //by set them up i mean make their variables- maybe a class? java does it so why not lol
-    //wow maybe this is easier than i thought- since its a 2d array i can make them vert/horiz based on even or odd row #. selection will be wayyyy more easy than i thought it'd be 
     this.left;
     this.right;
     this.up;
@@ -40,7 +30,7 @@ export default class Yipee extends Phaser.Scene {
   preload() {
    console.log("preload start");
    this.load.image("background", new URL('../Team Notateam/YipeeAssets/Ybg.png', import.meta.url).href);
-   this.load.image("box", new URL('../Team Notateam/YipeeAssets/box.png', import.meta.url).href);
+   //this.load.image("box", new URL('../Team Notateam/YipeeAssets/box.png', import.meta.url).href);
    console.log("preload end");
   }
   create() {
@@ -50,9 +40,6 @@ export default class Yipee extends Phaser.Scene {
     this.createKeys();
     this.initArr();
     this.disArray();
-    // this.boundary = this.physics.add.sprite(1080-270, 720-180, "box" );
-    // this.makeAnims();
-    // this.boundary.setVisible = true;
     console.log("create end");
   }
   update() {
@@ -64,35 +51,30 @@ export default class Yipee extends Phaser.Scene {
       this.globalState.sendMessage(this.victory)
       this.sent = true
     }
-    //add ? : for the hover highlight thingy
     this.buttonHandlers.update();
-    //}
   }
   initArr(){
     this.arr =[["","",""], ["","",""], ["","",""], ["","",""], ["","",""] ];
     for(let i=0; i<5;i++){
       for (let j=0; j<3;j++){
-        //status--> 2= on, 1=hover, 0= off,
+        //status sel nosel
         if (i%2===0&&j%2===1){//h
           this.arr[i][j]= {
-            status: 0,
-            orien: "h",
             box: this.add.rectangle(w/2, l/2, 100, 50, 0xcdb4db) ,
-            outline: this.add.rectangle(w/2, l/2, 100, 50, 0xa2d2ff, false)
           }
+          this.arr[i][j].status="nosel";
+          this.arr[i][j].isHovering="n";
         }
         else if (i%2===1&&j%2===0){
           this.arr[i][j]= {
-          status: 0,
-          orien: "v",
           box: this.add.rectangle(w/2,l/2, 50, 100, 0xcdb4db) ,
-          outline: this.add.rectangle(w/2, l/2, 50, 100, 0xa2d2ff, false)
           }
+          this.arr[i][j].status = "nosel";
+          this.arr[i][j].isHovering = "n";
         }
       }
     }
   }
-  
   disArray(){
     let shortLen = 50;
     let longLen = 100;
@@ -124,19 +106,16 @@ export default class Yipee extends Phaser.Scene {
       this.arr[i+1][col].box.x= location.x;
       this.arr[i+1][col].box.y= location.y;
     }
+    this.updateColor();
   }
   createKeys(){
     //inputs
     this.up = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
     this.down = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
     this.left = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
-    this.right = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT
-    );
+    this.right = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
     this.select = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
-    //for the selection the little analog limbs will change outline to indicate highlight and selection will change fill color
-
   }
-
  keyInputs(){
   if (Phaser.Input.Keyboard.JustDown(this.up)) this.updateSelection(1);
   if (Phaser.Input.Keyboard.JustDown(this.down)) this.updateSelection(2);
@@ -148,6 +127,9 @@ export default class Yipee extends Phaser.Scene {
  updateInput(i) {
   let rLim = this.arr.length-1;
   let cLim = this.arr[0].length-1;
+  if (this.arr[this.cursor.r][this.cursor.c]!=''){
+    this.arr[this.cursor.r][this.cursor.c].isHovering = "n";
+  }
     switch(i){
       case 1:
         if (this.cursor.r===0){
@@ -155,7 +137,7 @@ export default class Yipee extends Phaser.Scene {
         }else{
           this.cursor.r-=1;
         }
-        console.log("up");
+        //console.log("up");
         break;
       case 2:
         if (this.cursor.r===rLim){
@@ -169,7 +151,7 @@ export default class Yipee extends Phaser.Scene {
           (this.cursor.r===2&&this.cursor.c===2)){
           this.cursor.r+=1;
         }
-        console.log("down");
+        //console.log("down");
         break;
       case 3:
         if (this.cursor.c===0){
@@ -181,7 +163,7 @@ export default class Yipee extends Phaser.Scene {
         (this.cursor.r===3&&this.cursor.c===1)){
           this.cursor.c-=1;
         }
-        console.log("left");
+        //console.log("left");
         break;
       case 4:
         if (this.cursor.c===cLim){
@@ -193,22 +175,49 @@ export default class Yipee extends Phaser.Scene {
         (this.cursor.r===3&&this.cursor.c===1)){
           this.cursor.c+=1;
         }
-        console.log("right");
+        //console.log("right");
         break;
     }
-    console.log("row: " + this.cursor.r );
-    console.log("column: " + this.cursor.c);
+    this.updateColor();
+    // console.log("row: " + this.cursor.r +"\ncolumn: " + this.cursor.c);
+    if (this.arr[this.cursor.r][this.cursor.c]!=''){
+      this.arr[this.cursor.r][this.cursor.c].isHovering = "y";
+    //   console.log("hovering: " + this.arr[this.cursor.r][this.cursor.c].isHovering);
+    //   console.log("sel or nosel: " + this.arr[this.cursor.r][this.cursor.c].status);
+     }//else{
+    //   console.log("empty");
+    // }
   }
-
   click(row,col){
-    console.log("clicky click ehhehehehe...");
-    if (this.arr[row][col]!=null){
-      this.arr[row][col].status = 2;
-    }else{
-      this.arr[row][col].status = 0;
+    //console.log("clicky click ehhehehehe...");
+    if (this.arr[row][col]!=''){
+      if (this.arr[row][col].status==="nosel"){
+        this.arr[row][col].status="sel";
+      }else{
+        this.arr[row][col].status="nosel";
+      }
+    }
+    this.updateColor();
+    //console.log("sel or nosel: " + this.arr[row][col].status);
+  }
+  updateColor(){
+    for (let i= 0; i<this.arr.length; i++){
+      for (let j=0; j<this.arr[0].length;j++){
+        let box = this.arr[i][j]
+        // if (box!=''){
+        //   if (box.status==="sel"&&box.isHovering==="y"){
+        //     console.log("outline+blue");
+        //   } else if(box.status==="sel"&&box.isHovering==="n"){
+        //     console.log("no outline+blue");
+        //   } else if(box.status==="nosel"&&box.isHovering==="y"){
+        //     console.log("outline+red");
+        //   } else {
+        //     console.log("no outline+red");
+        //   }
+        // }
+      }
     }
   }
-
   startGamePad(){
     if (this.input.gamepad.total) {
       this.gamePad = this.input.gamepad.pad1;
@@ -216,7 +225,6 @@ export default class Yipee extends Phaser.Scene {
       console.log(this.gamePad);
     }
   }
-
   initGamePad() {
     this.buttonHandlers.addPad(
       () => this.gamePad.leftStick.y===-1, 
@@ -237,12 +245,5 @@ export default class Yipee extends Phaser.Scene {
   makeAnims() {
     console.log('reachme 00');
   }
-  // trapezoid (num){
-  //   var faceDown = [
-  //     new Phaser.Math.Vector2(25,50),   // Top-left point
-  //     new Phaser.Math.Vector2(75,50),   // Top-right point
-  //     new Phaser.Math.Vector2(100,0),   // Bottom-right point
-  //     new Phaser.Math.Vector2(0,0)    // Bottom-left point
-  // ];
-  // }
 }
+// :)
