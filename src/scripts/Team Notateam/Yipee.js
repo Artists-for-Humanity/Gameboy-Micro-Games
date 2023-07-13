7cd0ffb75c7dba6387774306626fabef6d3bbfdf
Serenity <swilliams314159@gmail.com>
2023-07-13 16:57:28 -0400

    FINALLY GOT USER INPUT
    need to get the rest of the assets such as the text prompt and another thing in my game, as well as program how the user wins. also gotta figure out diagonal.

--- src/scripts/Team Notateam/Yipee.js
+++ src/scripts/Team Notateam/Yipee.js
@@ -3,6 +3,7 @@ import ButtonPressHandlers from '../ButtonPressHandlers';
 import eventsCenter from "../EventsCenter";
 let l = 720;
 let w = 1080;
+
 export default class Yipee extends Phaser.Scene {
   constructor() {
     console.log("constructor start");
@@ -29,119 +30,102 @@ export default class Yipee extends Phaser.Scene {
   }
   preload() {
    console.log("preload start");
-   this.load.image("background", new URL('src/scripts/Team Notateam/YipeeAssets/sky(10).png ', import.meta.url).href);
-   this.load.image("cat", new URL('src/scripts/Team Notateam/YipeeAssets/orangeCat copy.png', import.meta.url).href); 
-   this.load.spritesheet("up", new URL('src/scripts/Team Notateam/YipeeAssets/upLimb.png', import.meta.url).href,{ frameWidth:679/4, frameHeight:42 });
-   this.load.spritesheet("down", new URL('src/scripts/Team Notateam/YipeeAssets/downLimb.png', import.meta.url).href,{ frameWidth:679/4, frameHeight:42});
-   this.load.spritesheet("left", new URL('src/scripts/Team Notateam/YipeeAssets/leftLimb.png', import.meta.url).href,{ frameWidth:168/4, frameHeight:169});
-   this.load.spritesheet("right", new URL('src/scripts/Team Notateam/YipeeAssets/rightLimb.png', import.meta.url).href,{ frameWidth:168/4, frameHeight:169});
-   this.load.spritesheet("center", new URL('src/scripts/Team Notateam/YipeeAssets/centerLimb.png', import.meta.url).href,{ frameWidth:636/4, frameHeight:41});
+   this.load.image("background", new URL('YipeeAssets/sky.png ', import.meta.url).href);
+   this.load.image("cat", new URL('YipeeAssets/orangeCat.png', import.meta.url).href); 
+   this.load.spritesheet("up", new URL('YipeeAssets/upLimb.png', import.meta.url).href,{ frameWidth:169, frameHeight:42 });
+  //  this.load.spritesheet("down", new URL('YipeeAssets/downLimb.png', import.meta.url).href,{ frameWidth:169, frameHeight:42});
+  //  this.load.spritesheet("left", new URL('YipeeAssets/leftLimb.png', import.meta.url).href,{ frameWidth:42, frameHeight:169});
+   this.load.spritesheet("right", new URL('YipeeAssets/rightLimb.png', import.meta.url).href,{ frameWidth:42, frameHeight:169});
+   this.load.spritesheet("center", new URL('YipeeAssets/centerLimb.png', import.meta.url).href,{ frameWidth:159, frameHeight:41});
    console.log("preload end");
   }
+
   create() {
     console.log("create start");
     this.started= true;
     this.add.image(540, 360, "background");
+    this.add.image(249/2+30,l-30-295/2, "cat");
     this.createKeys();
     this.initArr();
-    this.disArray();
     console.log("create end");
   }
   update() {
+    console.log("update start");
     //if(this.started){
     this.buttonHandlers.update();
+    this.updateColor();
     if (!this.gamePad) this.startGamePad();
     if(this.gameOver && !this.sent){
       this.globalState.timerMessage('stop_timer')
       this.globalState.sendMessage(this.victory)
       this.sent = true
     }
-    this.buttonHandlers.update();
+    
+  //}
   }
   initArr(){
     this.arr =[["","",""], ["","",""], ["","",""], ["","",""], ["","",""] ];
     for(let i=0; i<5;i++){
       for (let j=0; j<3;j++){
-        //status sel nosel
+        //stat sel nosel
         let b = this.arr[i][j];
-        if (i%2===0&&j%2===1){//h
+        if (i%2===0&&j===1){//h
           switch(i){
             case 0:
-              b = {
-                limb : this.physics.add.sprite(w/2,l/2, "up", 0),
-              }
-              b.isHovering = "n";
-              b.status = "nosel";
+              this.arr[i][j] = {};
+              this.arr[i][j].limb = this.physics.add.sprite(w-120-679/8,l-60-21-169*2, "up", 0);
+              this.arr[i][j].isHovering = "n";
+              this.arr[i][j].stat = "nosel";
               break;
             case 2:
-              b = {
-                limb : this.physics.add.sprite(w/2,l/2, "center", 0),
-              }
-              b.isHovering = "n";
-              b.status = "nosel";
+              this.arr[i][j] = {};      
+              this.arr[i][j].limb = this.physics.add.sprite(w-120-679/8,l-60-21-169, "center", 0);
+              this.arr[i][j].isHovering = "n";
+              this.arr[i][j].stat = "nosel";
               break;
             case 4:
-              b = {
-                limb : this.physics.add.sprite(w/2,l/2, "down", 0),
-              }
-              b.isHovering = "n";
-              b.status = "nosel";
+              this.arr[i][j] = {};
+              this.arr[i][j].limb= this.physics.add.sprite(w-120-679/8,l-60-21, "up", 0);
+              this.arr[i][j].limb.flipY = true;
+              this.arr[i][j].isHovering = "n";
+              this.arr[i][j].stat = "nosel";
               break; 
           }
         }
         else if (i%2===1&&j%2===0){
           switch(j){
             case 0:
-              b = {
-                limb : this.physics.add.sprite(w/2,l/2, "left", 0),
+              if (i===1){
+                this.arr[i][j] = {};
+                this.arr[i][j].limb = this.physics.add.sprite(w-120-679/4, l-60-21-169*2+169/2, "right", 0);
+                this.arr[i][j].limb.flipX = true;
+                this.arr[i][j].isHovering = "n";
+                this.arr[i][j].stat = "nosel";
+              } else{
+                this.arr[i][j] = {};
+                this.arr[i][j].limb = this.physics.add.sprite(w-120-679/4, l-60-21-169/2, "right", 0);
+                this.arr[i][j].limb.flipX = true;
+                this.arr[i][j].isHovering = "n";
+                this.arr[i][j].stat = "nosel";
               }
-              b.isHovering = "n";
-              b.status = "nosel";
               break;
             case 2:
-              b = {
-                limb : this.physics.add.sprite(w/2,l/2, "right", 0),
+              if (i===1){
+                this.arr[i][j] = {};
+                this.arr[i][j].limb = this.physics.add.sprite(w-120,l-60-21-169*2+169/2, "right", 0);
+                this.arr[i][j].isHovering = "n";
+                this.arr[i][j].stat = "nosel";
+              } else {
+                this.arr[i][j] = {};
+                this.arr[i][j].limb = this.physics.add.sprite(w-120,l-60-21-169/2, "right", 0);
+                this.arr[i][j].isHovering = "n";
+                this.arr[i][j].stat = "nosel";
               }
-              b.isHovering = "n";
-              b.status = "nosel";
               break;
           }
         }
       }
     }
-  }
-  disArray(){
-    let shortLen = 50;
-    let longLen = 100;
-    let origin = {
-      x: w*(2.4/3),
-      y: l*(1.5/3)
-    }
-    let location = {
-      x: origin.x,
-      y: origin.y
-    }
-    location.x+=shortLen+longLen/2;
-    location.y+=shortLen/2;
-    this.arr[0][1].limb.x=location.x;
-    this.arr[0][1].limb.y=location.y;
-    for (let i = 1; i<5;i+=2){
-      let col = 0;
-      location.y+= shortLen/2+longLen/2;
-      location.x-= longLen/2+shortLen/2;
-      this.arr[i][col].limb.x= location.x;
-      this.arr[i][col].limb.y= location.y;
-      location.x+=shortLen/2+longLen+shortLen/2;
-      col+=2;
-      this.arr[i][col].limb.x= location.x
-      this.arr[i][col].limb.y= location.y;
-      location.x -=shortLen/2+longLen/2;
-      location.y+=longLen/2+shortLen/2;
-      col-=1;
-      this.arr[i+1][col].limb.x= location.x;
-      this.arr[i+1][col].limb.y= location.y;
-    }
-    this.updateColor();
   }
   createKeys(){
     //inputs
@@ -162,7 +146,7 @@ export default class Yipee extends Phaser.Scene {
  updateInput(i) {
   let rLim = this.arr.length-1;
   let cLim = this.arr[0].length-1;
-  if (this.arr[this.cursor.r][this.cursor.c]!=''){
+  if (this.arr[this.cursor.r][this.cursor.c].stat!=undefined){
     this.arr[this.cursor.r][this.cursor.c].isHovering = "n";
   }
     switch(i){
@@ -172,7 +156,7 @@ export default class Yipee extends Phaser.Scene {
         }else{
           this.cursor.r-=1;
         }
-        //console.log("up");
+        console.log("up");
         break;
       case 2:
         if (this.cursor.r===rLim){
@@ -186,7 +170,7 @@ export default class Yipee extends Phaser.Scene {
           (this.cursor.r===2&&this.cursor.c===2)){
           this.cursor.r+=1;
         }
-        //console.log("down");
+        console.log("down");
         break;
       case 3:
         if (this.cursor.c===0){
@@ -198,7 +182,7 @@ export default class Yipee extends Phaser.Scene {
         (this.cursor.r===3&&this.cursor.c===1)){
           this.cursor.c-=1;
         }
-        //console.log("left");
+        console.log("left");
         break;
       case 4:
         if (this.cursor.c===cLim){
@@ -210,41 +194,38 @@ export default class Yipee extends Phaser.Scene {
         (this.cursor.r===3&&this.cursor.c===1)){
           this.cursor.c+=1;
         }
-        //console.log("right");
+        console.log("right");
         break;
     }
-    this.updateColor();
-    // console.log("row: " + this.cursor.r +"\ncolumn: " + this.cursor.c);
-    if (this.arr[this.cursor.r][this.cursor.c]!=''){
+    console.log("row: " + this.cursor.r +"\ncolumn: " + this.cursor.c);
+    if (this.arr[this.cursor.r][this.cursor.c].stat!=undefined){
       this.arr[this.cursor.r][this.cursor.c].isHovering = "y";
-    //   console.log("hovering: " + this.arr[this.cursor.r][this.cursor.c].isHovering);
-    //   console.log("sel or nosel: " + this.arr[this.cursor.r][this.cursor.c].status);
-     }//else{
-    //   console.log("empty");
-    // }
+      console.log(this.arr[this.cursor.r][this.cursor.c]);
+     }else{
+      console.log(this.arr[this.cursor.r][this.cursor.c]);
+    }
   }
   click(row,col){
-    //console.log("clicky click ehhehehehe...");
-    if (this.arr[row][col]!=''){
-      if (this.arr[row][col].status==="nosel"){
-        this.arr[row][col].status="sel";
+    console.log("clicky click ehhehehehe...");
+    if (this.arr[row][col].stat!=undefined){
+      if (this.arr[row][col].stat==="nosel"){
+        this.arr[row][col].stat="sel";
       }else{
-        this.arr[row][col].status="nosel";
+        this.arr[row][col].stat="nosel";
       }
     }
-    this.updateColor();
-    //console.log("sel or nosel: " + this.arr[row][col].status);
+    console.log("sel or nosel: " + this.arr[row][col].stat);
   }
   updateColor(){
     for (let i= 0; i<this.arr.length; i++){
       for (let j=0; j<this.arr[0].length;j++){
         let box = this.arr[i][j]
-        if (box!=''){
-          if (box.status==="nosel"&&box.isHovering==="n"){
+        if (box.stat!=undefined){
+          if (box.stat==="nosel"&&box.isHovering==="n"){
             box.limb.setFrame(0);
-          } else if(box.status==="nosel"&&box.isHovering==="y"){
+          } else if(box.stat==="nosel"&&box.isHovering==="y"){
             box.limb.setFrame(1);
-          } else if(box.status==="sel"&&box.isHovering==="n"){
+          } else if(box.stat==="sel"&&box.isHovering==="n"){
             box.limb.setFrame(2);
           } else {
             box.limb.setFrame(3);
@@ -277,8 +258,8 @@ export default class Yipee extends Phaser.Scene {
       () => this.gamePad.buttons[0].pressed, 
       () => this.click(this.cursor.r, this.cursor.c));
   }
-  makeAnims() {
-    console.log('reachme 00');
-  }
+  // makeAnims() {
+  //   console.log('reachme 00');
+  // }
 }
 // :)
