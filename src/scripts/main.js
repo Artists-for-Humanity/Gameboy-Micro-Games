9ee744535e6c425116957d9e469c48e76412abd0
HandyDorceus <dorceus.handy@gmail.com>
2023-07-27 12:40:27 -0400

    prettier formating

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,32 +1,32 @@
 // Add imports for phaser library, scenes, and plugins
-import Phaser from "phaser";
-import phaserJuice from "./phaserJuice.js";
-import GlobalState from "./GlobalState";
-import GameOver from "./GameOver";
-import Emeowgency from "./8Bitties/Emeowgency";
-import ColorLab from "./8Bitties/ColorLab";
-import CarPump from "./DebugginDaOpps/CarPump";
-import TrashSort from "./DebugginDaOpps/TrashSort";
-import Cannon from "./DebugginDaOpps/Cannon";
-import Lowest from "./TeamInflation/Lowest";
-import FrogJump from "./TeamInflation/FrogJump";
-import BetweenSpace from "./TeamInflation/BetweenSpace";
-import ColorPasscode from "./MicroGamers/ColorPasscode";
-import HideFromCat from "./MicroGamers/HideFromCat";
-import HitTheButton from "./MicroGamers/HitTheButton";
-import TugOWar from "./8Bitties/TugOWar";
-import FlySwat from "./8Bitties/FlySwat";
-import DrinkPour from "./8Bitties/DrinkPour";
-import SockToss from "./Team Notateam/SockToss";
-import CutScreen from "./Team Notateam/CutScreen";
-import MainMenu from "./MainMenu";
-import Timer from "./Timer";
-import MarcyMunch from "./8Bitties/MarcyMunch";
-import CircleJump from "./TeamInflation/CircleJump";
-import Factory from "./Fairway/factoryScripts/factoryScenes/Factory";
-import HiScoreScene from "./Hi-Score";
+import Phaser from 'phaser';
+import phaserJuice from './phaserJuice.js';
+import GlobalState from './GlobalState';
+import GameOver from './GameOver';
+import Emeowgency from './8Bitties/Emeowgency';
+import ColorLab from './8Bitties/ColorLab';
+import CarPump from './DebugginDaOpps/CarPump';
+import TrashSort from './DebugginDaOpps/TrashSort';
+import Cannon from './DebugginDaOpps/Cannon';
+import Lowest from './TeamInflation/Lowest';
+import FrogJump from './TeamInflation/FrogJump';
+import BetweenSpace from './TeamInflation/BetweenSpace';
+import ColorPasscode from './MicroGamers/ColorPasscode';
+import HideFromCat from './MicroGamers/HideFromCat';
+import HitTheButton from './MicroGamers/HitTheButton';
+import TugOWar from './8Bitties/TugOWar';
+import FlySwat from './8Bitties/FlySwat';
+import DrinkPour from './8Bitties/DrinkPour';
+import SockToss from './Team Notateam/SockToss';
+import CutScreen from './Team Notateam/CutScreen';
+import MainMenu from './MainMenu';
+import Timer from './Timer';
+import MarcyMunch from './8Bitties/MarcyMunch';
+import CircleJump from './TeamInflation/CircleJump';
+import Factory from './Fairway/factoryScripts/factoryScenes/Factory';
+import HiScoreScene from './Hi-Score';
 import WhereisWilly from "./8Bitties/Where'sWilly.js";
-import PenguinSlide from "./NewGames/PenguinSlide.js";
+import PenguinSlide from './NewGames/PenguinSlide.js';
 // Set configuration for phaser game instance
 const juice = new phaserJuice(this);
 
@@ -35,10 +35,10 @@ const config = {
   width: 1080,
   height: 720,
   transparent: false,
-  backgroundColor: "#4488aa",
+  backgroundColor: '#4488aa',
   // Add physics, arcade, scene, and audio
   physics: {
-    default: "arcade",
+    default: 'arcade',
     arcade: {
       gravity: {
         y: 0,
@@ -47,11 +47,10 @@ const config = {
     },
   },
   input: {
-    gamepad: true
+    gamepad: true,
   },
   scene: [
     MainMenu,
-    CutScreen,
     WhereisWilly,
     Timer,
     MarcyMunch,
@@ -80,15 +79,16 @@ const config = {
     disableWebAudio: true,
   },
   plugins: {
-    global: [{
-      key: "GlobalState",
-      plugin: GlobalState,
-      start: false,
-      mapping: "globalState",
-    },],
+    global: [
+      {
+        key: 'GlobalState',
+        plugin: GlobalState,
+        start: false,
+        mapping: 'globalState',
+      },
+    ],
   },
 };
 
 // Initialize game instance
 new Phaser.Game(config);
-
