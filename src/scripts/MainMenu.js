0eaca106548f8e56ef631bb274664b26953330a4
HandyDorceus <dorceus.handy@gmail.com>
2023-07-27 12:24:58 -0400

    update MainMenu add endless button image

--- src/scripts/MainMenu.js
+++ src/scripts/MainMenu.js
@@ -1,5 +1,5 @@
 import ButtonPressHandlers from './ButtonPressHandlers';
-
+import phaserJuice from './phaserJuice';
 import eventsCenter from './EventsCenter';
 import HiScoreScene from './Hi-Score';
 const X = 1080;
@@ -13,6 +13,7 @@ export default class MainMenu extends Phaser.Scene {
       visible: false,
       key: 'MainMenu',
     });
+    // console.log("Running Constructor");
 
     // an array of sprites
     this.btns = [];
@@ -30,21 +31,23 @@ export default class MainMenu extends Phaser.Scene {
     this.left;
     this.right;
     this.action;
+    this.buttonHandlers = new ButtonPressHandlers();
 
     this.sent = false;
 
     this.wobbleDir = false;
-
+    this.alphaVal = false;
     this.wobbleTimer = 0;
 
-    this.buttonHandlers = new ButtonPressHandlers();
+    this.buttonHandlers;
     this.gamePad = null;
-
+    this.pass = true;
     this.high = HiScoreScene;
   }
 
   preload() {
-    console.log('running main menu 00000');
+    // console.log('running main menu 00000');
+
     this.listOfGames = [
       'Emeowgency',
       'ColorLab',
@@ -60,7 +63,8 @@ export default class MainMenu extends Phaser.Scene {
       'FlySwat',
       'DrinkPour',
     ];
-
+    this.load.image('green1', new URL('gameAssets/greenGradiant.png', import.meta.url).href);
+    this.load.image('green2', new URL('gameAssets/solidGreen.png', import.meta.url).href);
     this.load.image('bg1', new URL('globalAssets/title_screen.png', import.meta.url).href);
     this.load.spritesheet('play', new URL('gameAssets/play_btn.png', import.meta.url).href, {
       frameWidth: 239,
@@ -71,12 +75,16 @@ export default class MainMenu extends Phaser.Scene {
       frameHeight: 117,
     });
     this.load.image('finger', new URL('gameAssets/finger.png', import.meta.url).href);
+    this.load.image('endless', new URL('gameAssets/EButton_Large.png', import.meta.url).href);
   }
 
   create() {
+    this.resetMainMenu();
     this.add.image(X / 2, Y / 2, 'bg1');
     this.btns.push(this.physics.add.sprite(X / 8, Y * 0.9, 'play'));
+    this.btns.push(this.physics.add.image((4 * X) / 8, Y * 0.9, 'endless').setScale(0.45));
     this.btns.push(this.physics.add.sprite((7 * X) / 8, Y * 0.9, 'score'));
+    this.greenSlip = this.add.image(this.btns[0].x, this.btns[0].y, 'green2').setScale(0.4);
 
     this.fingerIcon = this.add
       .image(this.btns[0].x, this.btns[0].y - 117, 'finger')
@@ -86,17 +94,30 @@ export default class MainMenu extends Phaser.Scene {
 
     this.right = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
     this.action = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
-    this.animationBuilder();
-    this.updateSelection();
+
+    this.juice = new phaserJuice(this);
   }
 
   update() {
-    // this.buttonPresses();
-    this.verticalWobble(this.fingerIcon, 0.5, 4);
+    this.verticalWobble(this.fingerIcon, 0.8, 4);
+    this.alterOppacity(this.greenSlip, 5 / 250);
+
     this.buttonHandlers.update();
     if (!this.gamePad) this.startGamePad();
+    if (!this.gamePad) this.makeKeyboardKeys();
   }
 
+  makeKeyboardKeys() {
+    this.left = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+    this.right = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+    this.space = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+    this.keyBoardInputs();
+  }
+  keyBoardInputs() {
+    if (Phaser.Input.Keyboard.JustDown(this.left)) this.updateSelection(-1);
+    if (Phaser.Input.Keyboard.JustDown(this.right)) this.updateSelection(1);
+    if (Phaser.Input.Keyboard.JustDown(this.space)) this.buttonEvents();
+  }
   startGamePad() {
     if (this.input.gamepad.total) {
       this.gamePad = this.input.gamepad.pad1;
@@ -131,45 +152,105 @@ export default class MainMenu extends Phaser.Scene {
   }
 
   updateSelection(input) {
-    this.btns[this.fingerPos].anims.stop().setFrame(0);
-
+    //left
     if (input === -1) {
       this.fingerPos === 0 ? (this.fingerPos = this.btns.length - 1) : this.fingerPos--;
     }
+    //right
     if (input === 1) {
       this.fingerPos === this.btns.length - 1 ? (this.fingerPos = 0) : this.fingerPos++;
     }
 
     this.fingerIcon.x = this.btns[this.fingerPos].x;
-    this.btns[this.fingerPos].anims.play(this.animations[this.fingerPos]);
+    this.greenSlip.x = this.btns[this.fingerPos].x;
   }
 
   buttonEvents() {
+    console.log(this.fingerPos);
     switch (this.fingerPos) {
       case 0: // Play
         this.playGame();
         // this.scene.start('MarcyMunch');
         break;
-      case 1: // Scores
+      case 1:
+        // this.resetMainMenu();
+        this.playEndless();
+        break;
+      case 2: // Scores
+        // this.resetMainMenu();
+        this.scene.stop('MainMenu');
         this.scene.start('HiScoreScene');
+
       default:
         break;
     }
   }
-
+  playEndless() {
+    if (!this.sent) {
+      eventsCenter.emit('start-endless');
+      this.sent = true;
+    }
+  }
   playGame() {
     if (!this.sent) {
-      this.scene.run('CutScreen');
+      console.log('cutscreenrunning .... ');
+      eventsCenter.emit('start-normal');
       //this.globalState.sendMessage(true)
       this.sent = true;
     }
   }
 
+  resetMainMenu() {
+    this.btns = [];
+    this.opac = 0.5;
+    this.animations = [];
+    this.fingerPos = 0;
+    this.left;
+    // this.int = 0;
+    this.right;
+    // this.pass = true;
+    this.action;
+    this.buttonHandlers = new ButtonPressHandlers();
+    this.sent = false;
+    this.wobbleDir = false;
+    this.wobbleTimer = 0;
+    this.gamePad = null;
+    // this.toggle = true;
+    this.alphaVal = false;
+    this.alphaTimer = false;
+    this.sign = true;
+  }
+  alterOppacity(obj, speed) {
+    if (this.sign === true) {
+      obj.setAlpha(this.opac);
+      this.opac -= speed;
+      if (this.opac <= 0) {
+        this.sign = false;
+      }
+    }
+    if (this.sign === false) {
+      obj.setAlpha(this.opac);
+      this.opac += speed;
+      if (this.opac >= 0.5) {
+        this.sign = true;
+      }
+    }
+  }
   animationBuilder() {
     this.animations.push(
       this.anims.create({
         key: 'play_btn',
         frames: 'play',
+        frameRate: 12,
+        repeat: -1,
+        yoyo: true,
+      })
+    );
+
+    this.animations.push(
+      this.anims.create({
+        key: 'score_btn',
+        frames: 'score',
         frameRate: 12,
         repeat: -1,
         yoyo: true,
