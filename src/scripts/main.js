d1d48b6505a2a2bd3a7f0b836b4b110ca755224e
HandyDorceus <dorceus.handy@gmail.com>
2023-07-27 11:38:06 -0400

    reformatted using prettier and removed cutscreen.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,34 +1,34 @@
 // Add imports for phaser library, scenes, and plugins
-import Phaser from "phaser";
-import GlobalState from "./GlobalState";
-import GameOver from "./GameOver";
-import Emeowgency from "./8Bitties/Emeowgency";
-import ColorLab from "./8Bitties/ColorLab";
-import CarPump from "./DebugginDaOpps/CarPump";
-import TrashSort from "./DebugginDaOpps/TrashSort";
-import phaserJuice from "./phaserJuice.js";
-import Cannon from "./DebugginDaOpps/Cannon";
-import Lowest from "./TeamInflation/Lowest";
-import FrogJump from "./TeamInflation/FrogJump";
-import BetweenSpace from "./TeamInflation/BetweenSpace";
-import ColorPasscode from "./MicroGamers/ColorPasscode";
-import HideFromCat from "./MicroGamers/HideFromCat";
-import HitTheButton from "./MicroGamers/HitTheButton";
-import TugOWar from "./8Bitties/TugOWar";
-import FlySwat from "./8Bitties/FlySwat";
-import DrinkPour from "./8Bitties/DrinkPour";
-import SockToss from "./Team Notateam/SockToss";
-import CutScreen from "./Team Notateam/CutScreen";
-import MainMenu from "./MainMenu";
-import Timer from "./Timer";
-import MarcyMunch from "./8Bitties/MarcyMunch";
-import CircleJump from "./TeamInflation/CircleJump";
-import Factory from "./Fairway/factoryScripts/factoryScenes/Factory";
-import HiScoreScene from "./Hi-Score";
+import Phaser from 'phaser';
+import GlobalState from './GlobalState';
+import GameOver from './GameOver';
+import Emeowgency from './8Bitties/Emeowgency';
+import ColorLab from './8Bitties/ColorLab';
+import CarPump from './DebugginDaOpps/CarPump';
+import TrashSort from './DebugginDaOpps/TrashSort';
+import phaserJuice from './phaserJuice.js';
+import Cannon from './DebugginDaOpps/Cannon';
+import Lowest from './TeamInflation/Lowest';
+import FrogJump from './TeamInflation/FrogJump';
+import BetweenSpace from './TeamInflation/BetweenSpace';
+import ColorPasscode from './MicroGamers/ColorPasscode';
+import HideFromCat from './MicroGamers/HideFromCat';
+import HitTheButton from './MicroGamers/HitTheButton';
+import TugOWar from './8Bitties/TugOWar';
+import FlySwat from './8Bitties/FlySwat';
+import DrinkPour from './8Bitties/DrinkPour';
+import SockToss from './Team Notateam/SockToss';
+import MainMenu from './MainMenu';
+import Timer from './Timer';
+import MarcyMunch from './8Bitties/MarcyMunch';
+import CircleJump from './TeamInflation/CircleJump';
+import Factory from './Fairway/factoryScripts/factoryScenes/Factory';
+import HiScoreScene from './Hi-Score';
 import WhereisWilly from "./8Bitties/Where'sWilly.js";
-import EndlessCutScreen from "./Team Notateam/newCutScreen.js";
-import newCutScreen from "./Team Notateam/newCutScreen.js";
-import fruitBasket from "./8Bitties/fruitBasket.js";
+import EndlessCutScreen from './Team Notateam/newCutScreen.js';
+import newCutScreen from './Team Notateam/newCutScreen.js';
+import fruitBasket from './8Bitties/fruitBasket.js';
+
 // Set configuration for phaser game instance
 const juice = new phaserJuice(this);
 
@@ -37,11 +37,11 @@ const config = {
   width: 1080,
   height: 720,
   transparent: false,
-  backgroundColor: "black",
+  backgroundColor: 'black',
 
   // Add physics, arcade, scene, and audio
   physics: {
-    default: "arcade",
+    default: 'arcade',
     arcade: {
       gravity: {
         y: 0,
@@ -50,7 +50,7 @@ const config = {
     },
   },
   input: {
-    gamepad: true
+    gamepad: true,
   },
   scene: [
     newCutScreen,
@@ -65,7 +65,7 @@ const config = {
     BetweenSpace,
     CircleJump,
     Lowest,
-    Factory, 
+    Factory,
     FrogJump,
     TugOWar,
     FlySwat,
@@ -84,15 +84,16 @@ const config = {
   },
 
   plugins: {
-    global: [{
-      key: "GlobalState",
-      plugin: GlobalState,
-      start: false,
-      mapping: "globalState",
-    },],
+    global: [
+      {
+        key: 'GlobalState',
+        plugin: GlobalState,
+        start: false,
+        mapping: 'globalState',
+      },
+    ],
   },
 };
 
 // Initialize game instance
 new Phaser.Game(config);
-
